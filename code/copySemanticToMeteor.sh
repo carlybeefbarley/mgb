fd72f3011799494798c832397f69b0704c1dbb4f
David Golds <david.p.golds@gmail.com>
2016-01-04 11:36:46 -0800

    improved comments regarding semantic-ui inclusion

--- code/copySemanticToMeteor.sh
+++ code/copySemanticToMeteor.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# This is only needed if you wish to refresh the sematic ui build
+
+# See http://semantic-ui.com/introduction/getting-started.html
+# Specifically, to get the semantic/ folder, we had invoked
+#  npm install semantic-ui --save
+#  cd semantic/
+#  gulp build
+
+# If that works ok, then the following is a convenient way to make these resoures available 
+#  and avoid a lot of className={style.ui + " " + style.segment} etc
+
 cp ./semantic/dist/semantic.{js,css} ./meteor_core/client
 mkdir ./meteor_core/public
 cp -r ./semantic/dist/themes ./meteor_core/public/themes

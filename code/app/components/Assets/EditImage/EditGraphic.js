e8d2dfdeaa71687e5854e80c59dcd16e887f44a1
David Golds <david.p.golds@gmail.com>
2016-01-11 13:45:06 -0800

    Sidebar for Image edit

--- code/app/components/Assets/EditImage/EditGraphic.js
+++ code/app/components/Assets/EditImage/EditGraphic.js
@@ -10,32 +10,69 @@ export default class EditGraphic extends React.Component {
   }
 
 
+  componentDidMount() {
+    $(this.refs.mySidebar).sidebar( {
+      transition: 'push',
+      context: $(this.refs.parentOfSidebar)
+    });
+
+}
+
+  handleToggleSidebar()
+  {
+    debugger;
+    $(this.refs.mySidebar).sidebar('toggle');
+  }
+
+
   render() {
 
     let asset = this.props.asset;
 
     return (
-      <div className="ui segment inverted">
-        <p>Edit {asset.kind} '{asset.name}' BABY</p>
-        <div className="ui segments">
-          <div className="ui segment">
-            <p>The</p>
+      <div>
+      <div ref="parentOfSidebar" className="ui segment inverted">
+          <p onClick={this.handleToggleSidebar.bind(this)}>
+            <i className="home icon"/>
+            Edit {asset.kind} '{asset.name}'
+          </p>
+
+          <div ref="mySidebar" className="ui left vertical inverted icon labeled very  thin sidebar menu visible">
+            <a className="item">
+              <i className="home icon"></i>
+              Home
+            </a>
+            <a className="item">
+              <i className="block layout icon"></i>
+              Shape
+            </a>
+            <a className="item">
+              <i className="smile icon"></i>
+              Stamps
+            </a>
           </div>
-          <div className="ui red segment">
-            <p>Fancy</p>
+
+          <div className="ui segments pushable">
+            <div className="ui segment">
+              <p>The</p>
+            </div>
+            <div className="ui red segment">
+              <p>Fancy</p>
+            </div>
+            <div className="ui blue segment">
+              <p>Editor</p>
+            </div>
+            <div className="ui green segment">
+              <p>Will</p>
+            </div>
+            <div className="ui yellow segment">
+              <p>Go</p>
+            </div>
+            <div className="ui orange segment">
+              <p>Here</p>
+            </div>
           </div>
-          <div className="ui blue segment">
-            <p>Editor</p>
-          </div>
-          <div className="ui green segment">
-            <p>Will</p>
-          </div>
-          <div className="ui yellow segment">
-            <p>Go</p>
-          </div>
-          <div className="ui orange segment">
-            <p>Here</p>
-          </div>
+
         </div>
       </div>
     )

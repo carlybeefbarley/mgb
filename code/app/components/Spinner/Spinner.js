b40e32e11bcb03b623267498628ef470b0da93bc
David Golds <david.p.golds@gmail.com>
2016-01-10 16:01:45 -0800

    replaced Spinner with Semantic Ui loader

--- code/app/components/Spinner/Spinner.js
+++ code/app/components/Spinner/Spinner.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import styles from './spinner.css';
 
 export default class Spinner extends Component {
 
   render() {
     return (
-      <div className={styles.spinner}>
+
+      <div className="ui padded segment">
+        <div className="horizontal divider"></div>
+        <div className="ui active dimmer">
+          <div className="ui indeterminate text loader"></div>
+        </div>
+        <p></p>
       </div>
     );
   }

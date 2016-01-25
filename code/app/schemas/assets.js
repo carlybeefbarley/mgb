f2726922465dca0fc5a6df42fc7bddf89513ef14
David Golds <david.p.golds@gmail.com>
2016-01-24 21:26:57 -1000

    Added asset types - audio, cutscene

--- code/app/schemas/assets.js
+++ code/app/schemas/assets.js
@@ -28,10 +28,12 @@ var schema = {
 // Info on each kind of asset, as the UI cares about it
 // .icon is as defined in http://semantic-ui.com/elements/icon.html
 export const AssetKinds = {
-  "search":  { name: "Search",  longName: "Search query", icon: "find", description: "Saved search query" },
-  "graphic": { name: "Graphic", longName: "Graphic", icon: "file image outline", description: "Images, Sprites, tiles, animations, backgrounds etc" },
-  "palette": { name: "Palette", longName: "Color Palette", icon: "block layout", description: "Color palette" },
-  "map":     { name: "Map",     longName: "Game Level Map", icon: "marker", description: "Map used in a game" },
+  "search":  { name: "Search",  longName: "Search query",    icon: "find", description: "Saved search query" },
+  "graphic": { name: "Graphic", longName: "Graphic",         icon: "file image outline", description: "Images, Sprites, tiles, animations, backgrounds etc" },
+  "palette": { name: "Palette", longName: "Color Palette",   icon: "block layout", description: "Color palette" },
+  "map":     { name: "Map",     longName: "Game Level Map",  icon: "marker", description: "Map used in a game" },
+  "cutscene":{ name: "Cutscene",longName: "Cut Scene",       icon: "spinner",description: "Cut scene used in a game" },
+  "audio":   { name: "Audio",   longName: "Audio sound",     icon: "announcement", description: "Sound Effect, song, voice etc"},
   "game":    { name: "Game",    longName: "Game definition", icon: "gamepad", description: "Game rules and definition"},
   // Helper function that handles unknown asset kinds and also appends ' icon' for convenience
   getIconClass: function (key) { return (AssetKinds.hasOwnProperty(key) ? AssetKinds[key].icon : "warning sign") + " icon"},

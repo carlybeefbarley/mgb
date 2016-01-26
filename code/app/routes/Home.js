04f0ab96c20a85f4d48faeebfb0b8078f684b1db
David Golds <david.p.golds@gmail.com>
2016-01-25 16:57:15 -1000

    Narrow home screen is better now

--- code/app/routes/Home.js
+++ code/app/routes/Home.js
@@ -12,8 +12,8 @@ export default class Home extends Component {
         <div className={styles.section1}>
           <div className={styles.heroGrid}>
             <div className={styles.heroColumn}>
-              <h1 className={styles.s1text}>MyGameBuilder v2</h1>
-              <h3 className={styles.s1text}>Make Games. Make Friends. Have Fun.</h3>
+              <h1 className={styles.s1text}>MyGameBuilder</h1>
+              <h3 className={styles.s1text}>Make Games.<br></br>Make Friends.<br></br>Have Fun.</h3>
               <Link to="/docs"><button className={styles.primary}>Docs</button></Link>
               <a href="http://mygamebuilder.com"><button className={styles.primary}>MGB</button></a>
             </div>
@@ -26,11 +26,9 @@ export default class Home extends Component {
             <div className={styles.listColumn}>
               <h4>Features</h4>
               <ul className={styles.listUl}>
-                <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Online Editing</li>
-                <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Collaborative game development</li>
+                <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Collaborative online game development</li>
                 <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Pro plans for advanced features and extra help</li>
-                <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Manage your games users communities</li>
-                <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Dashboard view of status and activity</li>
+                <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Track and Manage your game's user communities</li>
               </ul>
             </div>
             <div className={styles.listColumn}>
@@ -39,8 +37,6 @@ export default class Home extends Component {
                 <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Users, Teams and Orgs</li>
                 <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>User accounts are always free, teams/orgs have to pay</li>
                 <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Users can belong to many teams</li>
-                <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>People in orgs want to manage User roles</li>
-                <li className={styles.list}><span className={styles.iconList}><Icon size="1em" icon="check" /> </span>Teams have dashboards to manage things</li>
               </ul>
             </div>
           </div>

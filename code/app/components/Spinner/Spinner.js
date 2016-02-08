5c43b32014e822718fd534ddc67422dd565b431a
David Golds <david.p.golds@gmail.com>
2016-02-08 14:36:38 -0800

    Betterer spinner - shows again, but only after half a second

--- code/app/components/Spinner/Spinner.js
+++ code/app/components/Spinner/Spinner.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 
 export default class Spinner extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      hiddenInitially: true               // Used for delayed display
+    }
+  }
+
+  componentDidMount()
+  {
+    // Spinner will show after half a second, so it flickers less in most cases
+    setTimeout( () => { this.setState( {hiddenInitially: false})}, 500)
+  }
+
   render() {
     return (
-
-      <div className="ui padded segment">
-        <div className="horizontal divider"></div>
-        <div className="ui active" style={{minHeight: "300px"}}>
-          <div className="ui indeterminate text loader"></div>
+      <div className="ui segment" style={{minHeight: "15em"}}>
+        <div className={`ui ${this.state.hiddenInitially ? "" : "active"} inverted dimmer`} >
+          <div className="ui text indeterminate loader">Loading</div>
         </div>
         <p></p>
       </div>

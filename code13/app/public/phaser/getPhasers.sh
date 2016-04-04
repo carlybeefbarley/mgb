b8bc9916bfe449f875c171233ce31deb56c23f8e
David Golds <david.p.golds@gmail.com>
2016-04-04 14:13:12 -0700

    Added local copies of phaser (not using them yet though - still loading from cdn for now)

---
+++ code13/app/public/phaser/getPhasers.sh
@@ -0,0 +1,5 @@
+wget http://cdn.jsdelivr.net/phaser/2.2.2/phaser.js -O 2.2.2/phaser.js
+wget http://cdn.jsdelivr.net/phaser/2.2.2/phaser.min.js -O 2.2.2/phaser.min.js
+wget http://cdn.jsdelivr.net/phaser/2.4.6/phaser.js -O 2.4.6/phaser.js
+wget http://cdn.jsdelivr.net/phaser/2.4.6/phaser.min.js -O 2.4.6/phaser.min.js
+

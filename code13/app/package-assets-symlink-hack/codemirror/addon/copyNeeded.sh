1076f596e8f6b36cfda859633ddc6f56750bc0e1
David Golds <david.p.golds@gmail.com>
2016-04-06 12:28:36 -0700

    New codemirror has the updateArgs fix

---
+++ code13/app/package-assets-symlink-hack/codemirror/addon/copyNeeded.sh
@@ -0,0 +1,6 @@
+cp ../../../node_modules/codemirror/addon/dialog/dialog.css . 
+cp ../../../node_modules/codemirror/addon/fold/foldgutter.css .
+cp ../../../node_modules/codemirror/addon/lint/lint.css .
+cp ../../../node_modules/codemirror/addon/search/matchesonscrollbar.css .
+cp ../../../node_modules/codemirror/addon/hint/show-hint.css .
+cp ../../../node_modules/codemirror/addon/tern/tern.css .

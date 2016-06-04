1dff55f3222b188016a6e66c650c6129f1b14652
David Golds <david.p.golds@gmail.com>
2016-06-03 22:06:29 -0700

    Improved Navigate FlexPanel

--- code13/app/imports/components/FlexPanel/fpNavigate.js
+++ code13/app/imports/components/FlexPanel/fpNavigate.js
@@ -1,19 +1,34 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
-
 export default fpNavigate = React.createClass({
-  
   
   propTypes: {
     currUser:               PropTypes.object,             // Currently Logged in user. Can be null/undefined
     user:                   PropTypes.object,             // User object for context we are navigation to in main page. Can be null/undefined. Can be same as currUser, or different user
+    activity:               PropTypes.array.isRequired,   // An activity Stream passed down from the App and passed on to interested compinents
     flexPanelWidth:         PropTypes.string.isRequired   // Typically something like "200px". 
   },
 
+
   render: function () {
     
-    return <div class="ui row"><Link to="/">HOME</Link></div>
+    return  <div class="ui row">
+              <div className="ui list">
+                <div className="item">
+                  <i className="home icon"></i>
+                  <Link to="/" className="content">Home</Link>
+                </div>
+                <div className="item">
+                  <i className="users icon"></i>
+                  <Link to="/users" className="content">Users</Link>
+                </div>
+                <div className="item">
+                  <i className="pencil icon"></i>
+                  <Link to="/assets" className="content">All Assets</Link>
+                </div>
+              </div>
+            </div>
     
   }
 

6c887d51de5917757c92f76356c2b73fe3c2b768
Guntis <guntis.smaukstelis@gmail.com>
2016-06-07 15:40:32 +0300

    by default hidding preview canvases

--- code13/app/imports/components/Assets/EditGraphic/EditGraphic/SpriteLayers.js
+++ code13/app/imports/components/Assets/EditGraphic/EditGraphic/SpriteLayers.js
@@ -10,7 +10,7 @@ export default class SpriteLayers extends React.Component {
 	    this.state = {
 	    	allLayersHidden: false,
 	    	allLayersLocked: false,
-	    	isCanvasVisible: true,
+	    	isCanvasVisible: false,
 	    };
 	}
 

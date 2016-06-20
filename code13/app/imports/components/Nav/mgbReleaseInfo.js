8a22e0d5d1c173590fd0e99f4eb785d130e6f471
David Golds <david.p.golds@gmail.com>
2016-06-20 00:55:56 -0700

    More better info

--- code13/app/imports/components/Nav/mgbReleaseInfo.js
+++ code13/app/imports/components/Nav/mgbReleaseInfo.js
@@ -21,7 +21,7 @@ export default mgbReleaseInfo = {
       "id": {
         "ver":         "0.0.1",
         "state":       "alpha",
-        "iteration":   "615"
+        "iteration":   "617"
       },
 
       "releaseManagement": {

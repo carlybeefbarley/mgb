3f89e2cec702715516441373862960943e9c05d6
David Golds <david.p.golds@gmail.com>
2016-07-10 14:55:17 -0700

    Trying some React best practices

--- code13/app/imports/components/Assets/NewAsset/AssetCreateNew.js
+++ code13/app/imports/components/Assets/NewAsset/AssetCreateNew.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { AssetKinds, AssetKindKeys } from '/imports/schemas/assets';
-import { doesUserHaveRole } from '/imports/schemas/roles'
+import AssetCreateSelectKind from './AssetCreateSelectKind';
 
 
 export default AssetCreateNew = React.createClass({
@@ -18,21 +18,24 @@ export default AssetCreateNew = React.createClass({
 
   getInitialState: function () {
     return { 
-      selectedKind: "",     // "" or one of AssetKindKeys[]
-      newAssetName: ""      // "" or a valid assetName string
+      buttonActionPending: false,     // True after the button has been pushed. so it doesn't get pushed twice
+      selectedKind: "",               // "" or one of AssetKindKeys[]
+      newAssetName: ""                // "" or a valid assetName string
     }
   },
 
 
   render: function() {
+    const { currUser } = this.props
     const isAssetNameValid = (this.state.newAssetName !== "")   // TODO - some checks for crazy characters
     const isKindChosen = (this.state.selectedKind !== "")
     const isAssetReadyToCreate = isKindChosen && isAssetNameValid
     const chosenKindStr = isKindChosen ? AssetKinds[this.state.selectedKind].name : "Asset"
     const chosenNameStr = isAssetNameValid ? `"${this.state.newAssetName}"` : ""
-    const createButtonClassName = "ui primary" + (isAssetReadyToCreate ? " " : " disabled ") + "button"
+    const isButtonDisabled = this.state.buttonActionPending || !isAssetReadyToCreate
+    const createButtonClassName = "ui primary" + (isButtonDisabled ? " disabled " : " ") + "button"
     const createButtonTooltip = isAssetReadyToCreate ? "Click here to create your new Asset" : "You must choose a valid name and 'kind' for your new asset. You can rename it later if you wish, but you cannot change it's 'kind' later"
-
+    
     return (
       <div>
         <div title={createButtonTooltip}>
@@ -50,55 +53,16 @@ export default AssetCreateNew = React.createClass({
             </div>
           </div>
         </div>
+
         <div className="ui padded segment">
           <h4 className="ui header">Choose an Asset Kind</h4>        
-          <div className="ui items">
-            { this.renderAssetKindChoices() }
-          </div>
+          <AssetCreateSelectKind 
+            currUser={currUser} 
+            selectedKind={this.state.selectedKind} 
+            handleSelectAsset={this.handleSelectAssetKindClick} />
         </div>
       </div>
     )
-  },
-
-
-  renderAssetKindChoices: function() {
-    return AssetKindKeys.map((k) => {
-      const ak = AssetKinds[k]
-      const isActive = (k === this.state.selectedKind)
-
-      let sty = { 
-        backgroundColor: (isActive ? "rgba(0,255,0,0.15)" : "rgba(0,0,0,0.04)"),        
-        marginLeft: "6px", 
-        marginRight: "6px", 
-        marginTop: "6px", 
-        marginBottom:"6px", 
-        padding: "8px 8px 8px 8px"
-      }
-      if (ak.requiresUserRole)
-      {
-        if (!doesUserHaveRole(this.props.currUser, ak.requiresUserRole))
-          return null // Don't show
-        if (!isActive)
-          sty.backgroundColor = "rgba(0,0,255,0.04)"    // Blue for the special ones 
-      }
-
-      return (
-        <div className={"ui link item"} key={k} style={sty} onClick={this.handleSelectAssetKindClick.bind(this,k)}>
-          <div className="ui mini image">
-            <div className="center">
-              <i className={"huge " + ak.icon + " icon"}></i>
-            </div>
-          </div>
-          <div className="content" >
-            <div className="header">{ak.name}</div>
-            <div className="description">
-              { ak.description }
-              { ak.requiresUserRole && ` (Only available to ${ak.requiresUserRole} users)`}
-              </div>
-          </div>
-        </div>
-      )
-    });
   },
 
 
@@ -110,6 +74,7 @@ export default AssetCreateNew = React.createClass({
 
   handleCreateAssetClick: function()
   {
+    this.setState( { buttonActionPending: true})
     this.props.handleCreateAssetClick(this.state.selectedKind, this.state.newAssetName)
   }
 

445f9c198c0522e2f1f2f8518189011171d5b55d
Guntis <guntis.smaukstelis@gmail.com>
2016-07-13 15:17:40 +0300

    scale paste preview with scroll + shift

--- code13/app/imports/components/Assets/EditGraphic/GraphicTools/ToolPaste.js
+++ code13/app/imports/components/Assets/EditGraphic/GraphicTools/ToolPaste.js
@@ -29,8 +29,8 @@ const ToolPaste = {
       , pasteCanvas.height
       , 0
       , 0
-      , pasteCanvas.width
-      , pasteCanvas.height
+      , pasteCanvas.width * ToolPaste._scale
+      , pasteCanvas.height * ToolPaste._scale
     );
     drawEnv.previewCtx.rotate(-ToolPaste._angle );
     drawEnv.previewCtx.translate(-drawEnv.x, -drawEnv.y);
@@ -54,8 +54,8 @@ const ToolPaste = {
       , pasteCanvas.height
       , 0
       , 0
-      , pasteCanvas.width*drawEnv.scale
-      , pasteCanvas.height*drawEnv.scale
+      , pasteCanvas.width * drawEnv.scale * ToolPaste._scale
+      , pasteCanvas.height * drawEnv.scale * ToolPaste._scale
     );
     drawEnv.editCtx.rotate(-ToolPaste._angle );
     drawEnv.editCtx.translate(-drawEnv.x*drawEnv.scale, -drawEnv.y*drawEnv.scale);
@@ -64,12 +64,16 @@ const ToolPaste = {
 
   handleMouseWheel: ( drawEnv, wd ) => {
     if(drawEnv.event.altKey){
-        let direction = wd > 0 ? -1 : 1;
-        ToolPaste._angle += direction * (2*Math.PI) / 16;
-        ToolPaste.handleMouseMove(drawEnv);
+      let direction = wd > 0 ? -1 : 1;
+      ToolPaste._angle += direction * (2*Math.PI) / 16;
+      ToolPaste.handleMouseMove(drawEnv);
     }
     if(drawEnv.event.shiftKey){
-
+      let newScale = wd > 0 ? ToolPaste._scale / 2 : ToolPaste._scale * 2;
+      if(newScale >= 0.25 && newScale <=8){
+        ToolPaste._scale = newScale;
+      }
+      ToolPaste.handleMouseMove(drawEnv);
     }
     if(drawEnv.event.ctrlKey){
 

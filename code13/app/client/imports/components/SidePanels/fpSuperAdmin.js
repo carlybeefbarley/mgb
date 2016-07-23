1a48eab1c61a5bcb99d5e325c7da6236d4e075d8
David Golds <david.p.golds@gmail.com>
2016-07-23 12:14:31 -0700

    Handy links in our superadmin flexpanel for convenience

--- code13/app/client/imports/components/SidePanels/fpSuperAdmin.js
+++ code13/app/client/imports/components/SidePanels/fpSuperAdmin.js
@@ -9,9 +9,30 @@ export default fpSuperAdmin = React.createClass({
   },
 
   render: function () {    
+    const linkLi = (txt, url) => (<li><a target="_blank" href={url}>{txt}</a></li>)
+
     return  <div>
-              <div className="ui header">MAD POWERS</div>
-              <div className="ui sub header">...COMING SOON...</div>
+              <div className="ui header">SuperAdmin panel</div>
+              <div className="ui sub header">Team Quicklinks</div>
+              <ul>
+                { linkLi("localhost/v2 flipside", this.getFlipsideUrl()) }
+                { linkLi("TrackJs", "https://my.trackjs.com/messages") }
+                { linkLi("Galaxy", "https://galaxy.meteor.com/app/v2.mygamebuilder.com") }
+                { linkLi("Kadira", "https://ui.kadira.io/apps/e7zK3YN4QZijYhpmY/dashboard/overview") }
+                { linkLi("Slack", "https://devlapse.slack.com/messages/mgb-dev") }
+                { linkLi("Github", "https://github.com/devlapse/mgb") }
+                { linkLi("SemanticUI", "http://semantic-ui.com/") }
+                { linkLi("mLab telemetry", "https://mlab.com/realtime-dashboard?server=s-ds021730-a0") }
+                { linkLi("mLab cluster", "https://mlab.com/clusters/rs-ds021730") }
+                { linkLi("TimeTracker spreadsheet", "https://docs.google.com/spreadsheets/d/1dq1FjxoHfMl49R-dIoxi7kpTZFi6HCHlz78-9QUO-Ds/edit#gid=131993583")}
+              </ul>
             </div>
-  }  
+  },
+
+
+  getFlipsideUrl() {
+    const l = window.location
+    const newHost = l.host.startsWith("localhost") ? "https://v2.mygamebuilder.com" : "http://localhost:3000"
+    return `${newHost}${l.pathname}${l.search}`
+  }
 })

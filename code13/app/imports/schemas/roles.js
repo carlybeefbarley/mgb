6693a2e6e9c94631136e4448cf8d71a708b08ecb
David Golds <david.p.golds@gmail.com>
2016-07-31 18:50:23 -0700

    fix bug where isUserSuperAdmin() could return undefined

--- code13/app/imports/schemas/roles.js
+++ code13/app/imports/schemas/roles.js
@@ -6,10 +6,11 @@ export const roleSuperAdmin = "super-admin"
 
 export function doesUserHaveRole(user, roleStr) {
   // This is kind of simple while we only have one role group and one role type :)
-  return  user && 
-          user.permissions && 
-          user.permissions.length > 0 &&
-          user.permissions[0].roles[0] === roleStr
+  return  !!(
+    user && 
+    user.permissions && 
+    user.permissions.length > 0 &&
+    user.permissions[0].roles[0] === roleStr)
 }
 
 export function isUserSuperAdmin(user) {

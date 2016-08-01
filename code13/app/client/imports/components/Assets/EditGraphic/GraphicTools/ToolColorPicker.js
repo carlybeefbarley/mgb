4ad3ecc36d7e67df5487ef937225038b66be404b
Kaspars Stauzs <stauzs@gmail.com>
2016-08-01 13:49:41 +0300

    Toolbar cleanup / Custom component improvements - label

--- code13/app/client/imports/components/Assets/EditGraphic/GraphicTools/ToolColorPicker.js
+++ code13/app/client/imports/components/Assets/EditGraphic/GraphicTools/ToolColorPicker.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { PropTypes } from "react";
 
 let ToolColorPicker;
 class ColorPicker extends React.Component {
+
+  static propTypes = {
+    // it will show up when button is large enough to fit label
+    label: React.PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.string
+      ]).isRequired
+  }
+
   render(){
     return (
       <div
-        className="ui button hazPopup animate"
+        className="ui button animate"
         style={{position: "relative"}}
         data-title={ToolColorPicker.label}
         data-position="bottom left"
         ><i className={ToolColorPicker.icon + " icon"}></i>
+        {this.props.label}
       </div>)
   }
 }
@@ -22,7 +32,7 @@ ToolColorPicker = {
   shortcut: '',
   level: 1,
   simpleTool: true,   // this tool is not selectable, only action is on tool click
-  component: <ColorPicker key="ColorPicker" />
+  component: ColorPicker
 }
 
 export default ToolColorPicker

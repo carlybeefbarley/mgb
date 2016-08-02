c844ffe3b63db46217bddd9a65e615a94a04c5ef
David Golds <david.p.golds@gmail.com>
2016-08-01 17:09:32 -0700

    More admin links woot

--- code13/app/client/imports/components/SidePanels/fpSuperAdmin.js
+++ code13/app/client/imports/components/SidePanels/fpSuperAdmin.js
@@ -12,22 +12,33 @@ export default fpSuperAdmin = React.createClass({
   render: function () {    
     const linkLi = (txt, url) => (<li><a target="_blank" href={url}>{txt}</a></li>)
 
-    return  <div>
-              <div className="ui sub header">Team Quicklinks</div>
-              <ul>
-                { linkLi("localhost/v2 flipside", this.getFlipsideUrl()) }
-                { linkLi("TrackJs", "https://my.trackjs.com/messages") }
-                { linkLi("Galaxy", "https://galaxy.meteor.com/app/v2.mygamebuilder.com") }
-                { linkLi("Kadira", "https://ui.kadira.io/apps/e7zK3YN4QZijYhpmY/dashboard/overview") }
-                { linkLi("Slack", "https://devlapse.slack.com/messages/mgb-dev") }
-                { linkLi("Github", "https://github.com/devlapse/mgb") }
-                { linkLi("SemanticUI", "http://semantic-ui.com/") }
-                { linkLi("mLab telemetry", "https://mlab.com/realtime-dashboard?server=s-ds021730-a0") }
-                { linkLi("mLab cluster", "https://mlab.com/clusters/rs-ds021730") }
-                { linkLi("TimeTracker spreadsheet", "https://docs.google.com/spreadsheets/d/1dq1FjxoHfMl49R-dIoxi7kpTZFi6HCHlz78-9QUO-Ds/edit#gid=131993583")}
-                { linkLi("Stock Assets spreadsheet", "https://docs.google.com/spreadsheets/d/1LMmh_dTbBS51Nus8zLfXNAusoiUodBcJSMFzJz1agsg/edit#gid=1512032697")}
-              </ul>
-            </div>
+    return (
+      <div>
+        <div className="ui sub header">Dev Quicklinks</div>
+        <ul>
+          { linkLi("localhost/v2 flipside", this.getFlipsideUrl()) }
+          { linkLi("Slack", "https://devlapse.slack.com/messages/mgb-dev") }
+          { linkLi("Github", "https://github.com/devlapse/mgb") }
+          { linkLi("SemanticUI", "http://semantic-ui.com/") }
+          { linkLi("TimeTracker spreadsheet", "https://docs.google.com/spreadsheets/d/1dq1FjxoHfMl49R-dIoxi7kpTZFi6HCHlz78-9QUO-Ds/edit#gid=131993583")}
+        </ul>
+
+        <div className="ui sub header">Stock Assets Quicklinks</div>
+        <ul>
+          { linkLi("Stock Assets spreadsheet", "https://docs.google.com/spreadsheets/d/1LMmh_dTbBS51Nus8zLfXNAusoiUodBcJSMFzJz1agsg/edit#gid=1512032697")}
+          { linkLi("'!vault/Stock Assets' view", "https://v2.mygamebuilder.com/u/!vault/assets?_np=user&project=Stock+Assets")}
+        </ul>
+
+        <div className="ui sub header">Deployment/Monitoring Quicklinks</div>
+        <ul>
+          { linkLi("TrackJs (client errors)", "https://my.trackjs.com/messages") }
+          { linkLi("Galaxy (PaaS)", "https://galaxy.meteor.com/app/v2.mygamebuilder.com") }
+          { linkLi("Kadira (Meteor stats)", "https://ui.kadira.io/apps/e7zK3YN4QZijYhpmY/dashboard/overview") }
+          { linkLi("mLab telemetry", "https://mlab.com/realtime-dashboard?server=s-ds021730-a0") }
+          { linkLi("mLab cluster", "https://mlab.com/clusters/rs-ds021730") }
+        </ul>
+      </div>
+    )
   },
 
 

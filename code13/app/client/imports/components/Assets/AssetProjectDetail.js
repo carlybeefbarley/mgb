8f8aa3834bac66f4232ed198a0df0016500e76af
David Golds <david.p.golds@gmail.com>
2016-09-07 11:46:24 -0700

    Converted AssetProjectDetail to Pure/Stateless component

--- code13/app/client/imports/components/Assets/AssetProjectDetail.js
+++ code13/app/client/imports/components/Assets/AssetProjectDetail.js
@@ -1,30 +1,31 @@
 import React, { PropTypes } from 'react'
 
-export default AssetProjectDetail = React.createClass({
+const _propTypes = {
+  projectNames:  PropTypes.array.isRequired,
+  isDeleted:     PropTypes.bool.isRequired
+}
 
-  propTypes: {
-    projectNames:  PropTypes.array.isRequired,
-    isDeleted:     PropTypes.bool.isRequired
-  },
-  
-  render() {
-    const { projectNames, isDeleted } = this.props
+const AssetProjectDetail = (props) => {
+  const { projectNames, isDeleted } = props
 
-    const inProjectClassName = "ui small basic " + (isDeleted ? "red" : "grey") + " label"
-    const inProjectTitle = "Projects to which this asset belongs. " + 
-                            ( isDeleted ? 
-                              "(This asset is deleted. You can undelete this file from the Assets list. Use the Trashcan search filter to show deleted assets. Deleted items will be automatically purged after some number of days of non-use. Also, it is a bit weird, but we do let you edit deleted items.. why not?)" :
-                              "(Currently you can only assign Assets to Projects using the 'My Assets' page.)"
-                            )
+  const inProjectClassName = "ui small basic " + (isDeleted ? "red" : "grey") + " label"
+  const inProjectTitle = "Projects to which this asset belongs. " + 
+                          ( isDeleted ? 
+                            "(This asset is deleted. You can undelete this file from the Assets list. Use the Trashcan search filter to show deleted assets. Deleted items will be automatically purged after some number of days of non-use. Also, it is a bit weird, but we do let you edit deleted items.. why not?)" :
+                            "(Currently you can only assign Assets to Projects using the 'My Assets' page.)"
+                          )
 
-    return (
-      <div className={inProjectClassName} title={inProjectTitle}>
-        <span>
-          { isDeleted && "DELETED " }
-          <i className="ui icon sitemap" />
-          { (projectNames.length === 0 ? "(none)" :  projectNames.join(", ") ) }
-        </span>
-      </div>
-    )
-  }
-})
+  return (
+    <div className={inProjectClassName} title={inProjectTitle}>
+      <span>
+        { isDeleted && "DELETED " }
+        <i className="ui icon sitemap" />
+        { (projectNames.length === 0 ? "(none)" :  projectNames.join(", ") ) }
+      </span>
+    </div>
+  )
+}
+
+AssetProjectDetail.propTypes = _propTypes
+
+export default AssetProjectDetail

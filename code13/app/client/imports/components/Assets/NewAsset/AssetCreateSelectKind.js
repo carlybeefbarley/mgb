d014aa62d13e03395828a87941247a348ccb255b
David Golds <david.p.golds@gmail.com>
2016-09-09 17:18:55 -0700

    Much improved CreateAsset UI

--- code13/app/client/imports/components/Assets/NewAsset/AssetCreateSelectKind.js
+++ code13/app/client/imports/components/Assets/NewAsset/AssetCreateSelectKind.js
@@ -1,8 +1,6 @@
-import _ from 'lodash';
-import React, { PropTypes } from 'react';
-import { AssetKinds, AssetKindKeys } from '/imports/schemas/assets';
+import React, { PropTypes } from 'react'
+import { AssetKinds, AssetKindKeys } from '/imports/schemas/assets'
 import { doesUserHaveRole } from '/imports/schemas/roles'
-
 
 export default AssetCreateSelectKind = React.createClass({
   propTypes: {
@@ -12,48 +10,33 @@ export default AssetCreateSelectKind = React.createClass({
   },
 
   render: function() {
+    const { handleSelectAsset, currUser, selectedKind } = this.props
+    const activeAK = selectedKind ? AssetKinds[selectedKind] : null
+
     return (
-      <div className="ui items">
+      <div className="">
         { AssetKindKeys.map((k) => {
           const ak = AssetKinds[k]
-          const isActive = (k === this.props.selectedKind)
-
-          let sty = { 
-            backgroundColor: (isActive ? "rgba(0,255,0,0.15)" : "rgba(0,0,0,0.04)"),        
-            marginLeft: "6px", 
-            marginRight: "6px", 
-            marginTop: "6px", 
-            marginBottom:"6px", 
-            padding: "8px 8px 8px 8px"
-          }
+          const isActive = (k === selectedKind)
+          let sty = { width: "6em" }
           if (ak.requiresUserRole)
           {
-            if (!doesUserHaveRole(this.props.currUser, ak.requiresUserRole))
+            if (!doesUserHaveRole(currUser, ak.requiresUserRole))
               return null // Don't show
             if (!isActive)
-              sty.backgroundColor = "rgba(0,0,255,0.04)"    // Blue for the special ones 
+              sty.backgroundColor = "rgba(0,0,255,0.05)"    // Blue for the special ones 
           }
 
           return (
-            <div className={"ui link item"} key={k} style={sty} onClick={ () => {this.props.handleSelectAsset(k)}}>
-              <div className="ui mini image">
-                <div className="center">
-                  <i className={"huge " + ak.icon + " icon"}></i>
-                </div>
-              </div>
-              <div className="content" >
-                <div className="header">{ak.name}</div>
-                <div className="description">
-                  { ak.description }
-                  { ak.requiresUserRole && ` (Only available to ${ak.requiresUserRole} users)`}
-                  </div>
-              </div>
+            <div className={`ui icon ${isActive && "positive"} button`} key={k} style={sty} onClick={ () => {handleSelectAsset(k)}}>
+              <i className={ak.icon + " large icon"}></i>
+              <p style={{ marginTop: "4px" }}>{ak.name}</p>
             </div>
           )
         })
       }
+      { activeAK && <p> { <em>{activeAK.name}</em> + ": " + activeAK.description } </p> }
       </div>
     )
   }
-
 })

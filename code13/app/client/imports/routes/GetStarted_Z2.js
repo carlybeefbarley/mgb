e70fb91f60e87547ded3651a982629a76a4f9786
Nico Prins <nico@nicoprins.com>
2016-09-14 18:34:04 -0700

    Added new mascots to getting started

--- code13/app/client/imports/routes/GetStarted_Z2.js
+++ code13/app/client/imports/routes/GetStarted_Z2.js
@@ -10,68 +10,92 @@ export default class GetStarted_Z2 extends Component {
     const currUser = this.props.currUser
     const username = currUser ? currUser.profile.name : "guest"
 
+    const cardStyle = {
+      color: "#2e2e2e",
+    }
+
+    const mascotStyle = {
+      maxWidth: "8em",
+      paddingRight: "0.5em",
+      marginBottom: "0",
+    }
+
+    const headerStyle = {
+      marginTop: "0.15em",
+      marginBottom: "0.4em",
+    }
+
+    const descStyle = {
+      fontSize: "1.25em",
+      lineHeight: "1.5em",
+    }
+
     return (
-      // TODO: JSX does not seem to allow !important overrides, so either we need to add some global styling for
-      // narrow containers or handle the centering some other way
-      <div className="ui container" style={{padding: '0 52px'}}>
-        <div className="ui grid stackable" style={{marginTop: '1em'}}>
+      <div className="ui container" style={{maxWidth: '1050px !important', padding: '0 75px'}}>
+        <div className="ui grid stackable" style={{marginTop: '3.5em'}}>
           <div className="row">
             <div className="column">
               <h1 className="ui huge header" style={{fontSize: '2.5em'}}>
-                What would you like to do?
-                <em className="sub header">There are lots of skills needed to make a game</em>
+                What would you like to try?
+                <em className="sub header">There is a lot that goes into a game</em>
               </h1>
             </div>
           </div>
           <div className="row">
             <div className="column">
-              <div className="ui three doubling cards">
-                <a className="card">
-                  <div className="image" style={{padding: '1em'}}>
-                    <img src="images/mascots/slimy2.png" />
-                  </div>
+              <div className="ui two cards stackable skills">
+                <a className="card" style={cardStyle} href="tutorial.html">
                   <div className="content">
-                    <h3><i className="code icon" />&nbsp;Coding</h3>
+                    <div className="ui left floated image" style={mascotStyle}>
+                      <img src="/images/mascots/alien.png" />
+                    </div>
+                    <h2 style={headerStyle}><i className="code icon" />&nbsp;Coding</h2>
+                    <p style={descStyle}>Code using JavaScript and game engines like Phaser and others.</p>
                   </div>
                 </a>
-                <a className="card">
-                  <div className="image" style={{padding: '1em'}}>
-                    <img src="images/mascots/slimy2.png" />
-                  </div>
+                <a className="card" style={cardStyle} href="tutorial.html">
                   <div className="content">
-                    <h3><i className="paint brush icon" />&nbsp;Pixel art</h3>
+                    <div className="ui left floated image" style={mascotStyle}>
+                      <img src="/images/mascots/penguin.png" />
+                    </div>
+                    <h2 style={headerStyle}><i className="paint brush icon" />&nbsp;Pixel art</h2>
+                    <p style={descStyle}>Code using JavaScript and game engines like Phaser and others.</p>
                   </div>
                 </a>
-                <a className="card">
-                  <div className="image" style={{padding: '1em'}}>
-                    <img src="images/mascots/slimy2.png" />
-                  </div>
+                <a className="card" style={cardStyle} href="tutorial.html">
                   <div className="content">
-                    <h3><i className="music icon" />&nbsp;Music &amp; audio</h3>
+                    <div className="ui left floated image" style={mascotStyle}>
+                      <img src="/images/mascots/shark.png" />
+                    </div>
+                    <h2 style={headerStyle}><i className="music icon" />&nbsp;Audio &amp; music</h2>
+                    <p style={descStyle}>Code using JavaScript and game engines like Phaser and others.</p>
                   </div>
                 </a>
-                <a className="card">
-                  <div className="image" style={{padding: '1em'}}>
-                    <img src="images/mascots/slimy2.png" />
-                  </div>
+                <a className="card" style={cardStyle} href="tutorial.html">
                   <div className="content">
-                    <h3><i className="idea icon" />&nbsp;Game design</h3>
+                    <div className="ui left floated image" style={mascotStyle}>
+                      <img src="/images/mascots/samurai.png" />
+                    </div>
+                    <h2 style={headerStyle}><i className="idea icon" />&nbsp;Game design</h2>
+                    <p style={descStyle}>Code using JavaScript and game engines like Phaser and others.</p>
                   </div>
                 </a>
-                <a className="card">
-                  <div className="image" style={{padding: '1em'}}>
-                    <img src="images/mascots/slimy2.png" />
-                  </div>
+                <a className="card" style={cardStyle} href="tutorial.html">
                   <div className="content">
-                    <h3><i className="write icon" />&nbsp;Story writing</h3>
+                    <div className="ui left floated image" style={mascotStyle}>
+                      <img src="/images/mascots/vampire.png" />
+                    </div>
+                    <h2 style={headerStyle}><i className="write icon" />&nbsp;Story writing</h2>
+                    <p style={descStyle}>Code using JavaScript and game engines like Phaser and others.</p>
                   </div>
                 </a>
-                <a className="card">
-                  <div className="image" style={{padding: '1em'}}>
-                    <img src="images/mascots/slimy2.png" />
-                  </div>
+                <a className="card" style={cardStyle} href="tutorial.html">
                   <div className="content">
-                    <h3><i className="area chart icon" />&nbsp;Analytics</h3>
+                    <div className="ui left floated image" style={mascotStyle}>
+                      <img src="/images/mascots/slimy.png" />
+                    </div>
+                    <h2 style={headerStyle}><i className="area chart icon" />&nbsp;Analytics</h2>
+                    <p style={descStyle}>Code using JavaScript and game engines like Phaser and others.</p>
                   </div>
                 </a>
               </div>

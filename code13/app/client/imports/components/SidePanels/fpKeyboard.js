5ce97e93c140370ddad4aeb469e26438da7c4d11
David Golds <david.p.golds@gmail.com>
2016-09-24 11:03:54 -0700

    convert fpKeyboard to a pure functional component

--- code13/app/client/imports/components/SidePanels/fpKeyboard.js
+++ code13/app/client/imports/components/SidePanels/fpKeyboard.js
@@ -1,24 +1,18 @@
 import React, { PropTypes } from 'react'
 import KeyBindings from '/client/imports/components/Skills/Keybindings'
 
-
 // TODO: Allow these to be edited using https://github.com/florian/react-shortcut-chooser
 
 // TODO: Have a nicer way to catch keypresses in app, such as
 //       https://github.com/zzarcon/react-keypress   (kinda simple)
 //       https://github.com/avocode/react-shortcuts  (a real keypress manager)
 
-export default fpUsers = React.createClass({
-  
-  propTypes: {
-    currUser:               PropTypes.object,             // Currently Logged in user. Can be null/undefined
-    user:                   PropTypes.object,             // User object for context we are navigation to in main page. Can be null/undefined. Can be same as currUser, or different user
-    panelWidth:             PropTypes.string.isRequired   // Typically something like "200px". 
-  },
+const fpKeyboard = (props) => (
+  <KeyBindings commandContext="editor.text." currUser={props.currUser} />
+)
 
-  render: function () {
-    return (
-      <KeyBindings commandContext="editor.text." currUser={this.props.currUser} />
-    )
-  }  
-})
+fpKeyboard.propTypes = {
+  currUser:  PropTypes.object           // Currently Logged in user. Can be null/undefined
+}
+
+export default fpKeyboard

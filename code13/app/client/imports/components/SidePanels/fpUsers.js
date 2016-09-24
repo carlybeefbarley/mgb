ebcd5991da4ac5d00e4a6a745303dfa1e31446f7
David Golds <david.p.golds@gmail.com>
2016-09-24 10:59:35 -0700

    Cleanup/Simplify fpusers and make clicks work

--- code13/app/client/imports/components/SidePanels/fpUsers.js
+++ code13/app/client/imports/components/SidePanels/fpUsers.js
@@ -1,27 +1,9 @@
-import _ from 'lodash';
-import React, { PropTypes } from 'react';
-import UserListRoute from '/client/imports/routes/Users/List.js';
+import React from 'react'
+import UserListRoute from '/client/imports/routes/Users/List.js'
 
-export default fpUsers = React.createClass({
-  
-  propTypes: {
-    currUser:               PropTypes.object,             // Currently Logged in user. Can be null/undefined
-    user:                   PropTypes.object,             // User object for context we are navigation to in main page. Can be null/undefined. Can be same as currUser, or different user
-    panelWidth:             PropTypes.string.isRequired   // Typically something like "200px". 
-  },
-
-  handleClickUser: function(userId, userName)
-  {
-    // TODO
-  },
-
-  render: function () {
-    return (
-      <UserListRoute  
-        handleClickUser={this.handleClickUser}
-        initialLimit={20}
-        renderVertical={true} 
-        hideTitle={true}/>
-    )
-  }  
-})
+export default fpUsers = () => (
+  <UserListRoute  
+    initialLimit={20}
+    renderVertical={true} 
+    hideTitle={true}/>
+  )

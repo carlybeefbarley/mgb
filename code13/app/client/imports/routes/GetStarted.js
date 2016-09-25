b393239d54cbf09999baf36641bb6297d0568562
David Golds <david.p.golds@gmail.com>
2016-09-25 13:24:46 -0700

    Stardusted getStarted and made it data-driven

--- code13/app/client/imports/routes/GetStarted.js
+++ code13/app/client/imports/routes/GetStarted.js
@@ -1,109 +1,71 @@
-import _ from 'lodash';
-import React, {Component} from 'react';
-import styles from './home.css';
-import QLink from './QLink';
-import getStartedStyle from './GetStarted.css';
+import React from 'react'
+import styles from './home.css'
+import QLink from './QLink'
+import getStartedStyle from './GetStarted.css'
+import { Segment, Grid, Card, Header, Image, Icon } from 'stardust'
 
+const _notReallyWorkingYet = "These don't do anything yet.. but soon will! sorry for the psyche"
 
-export default class GetStarted extends Component {
-
-  render() {
-    const currUser = this.props.currUser
-    const username = currUser ? currUser.profile.name : "guest"
-
-    const cardStyle = {
-      color: "#2e2e2e",
-    }
-
-    const mascotStyle = {
-      maxWidth: "8em",
-      paddingRight: "0.5em",
-      marginBottom: "0",
-    }
-
-    const headerStyle = {
-      marginTop: "0.15em",
-      marginBottom: "0.4em",
-    }
-
-    const descStyle = {
-      fontSize: "1.25em",
-      lineHeight: "1.5em",
-    }
-
-    return (
-      <div className="ui basic padded segment slim" style={{margin: '0 auto'}}>
-        <div className="ui grid stackable" style={{marginTop: '3.5em'}}>
-          <div className="row">
-            <div className="column">
-              <h1 className="ui huge header" style={{fontSize: '2.5em'}}>
-                What would you like to try?
-                <em className="sub header">There's a lot to learn</em>
-              </h1>
-            </div>
-          </div>
-          <div className="row">
-            <div className="column">
-              <div className="ui two cards stackable skills">
-                <QLink className="card" style={cardStyle} to={`/assets/create`}>
-                  <div className="content">
-                    <div className="ui left floated image" style={mascotStyle}>
-                      <img src="/images/mascots/alien.png" />
-                    </div>
-                    <h2 style={headerStyle}><i className="code icon" />&nbsp;Coding</h2>
-                    <p style={descStyle}>Code using JavaScript and game engines like Phaser and others.</p>
-                  </div>
-                </QLink>
-                <QLink className="card" style={cardStyle} to={`/assets/create`}>
-                  <div className="content">
-                    <div className="ui left floated image" style={mascotStyle}>
-                      <img src="/images/mascots/penguin.png" />
-                    </div>
-                    <h2 style={headerStyle}><i className="paint brush icon" />&nbsp;Pixel art</h2>
-                    <p style={descStyle}>Learn to make animated sprites, spritesheets and tilemaps for games.</p>
-                  </div>
-                </QLink>
-                <QLink className="card" style={cardStyle} to={`/assets/create`}>
-                  <div className="content">
-                    <div className="ui left floated image" style={mascotStyle}>
-                      <img src="/images/mascots/samurai.png" />
-                    </div>
-                    <h2 style={headerStyle}><i className="music icon" />&nbsp;Audio &amp; music</h2>
-                    <p style={descStyle}>Bring engagement and mood to games through music and sound.</p>
-                  </div>
-                </QLink>
-                <QLink className="card" style={cardStyle} to={`/assets/create`}>
-                  <div className="content">
-                    <div className="ui left floated image" style={mascotStyle}>
-                      <img src="/images/mascots/slimy.png" />
-                    </div>
-                    <h2 style={headerStyle}><i className="idea icon" />&nbsp;Game design</h2>
-                    <p style={descStyle}>Design game levels, try different gameplay mechanics and balance your games.</p>
-                  </div>
-                </QLink>
-                <QLink className="card" style={cardStyle} to={`/assets/create`}>
-                  <div className="content">
-                    <div className="ui left floated image" style={mascotStyle}>
-                      <img src="/images/mascots/vampire.png" />
-                    </div>
-                    <h2 style={headerStyle}><i className="write icon" />&nbsp;Story writing</h2>
-                    <p style={descStyle}>Bring game stories to life through plot, character, narrative and dialog.</p>
-                  </div>
-                </QLink>
-                <QLink className="card" style={cardStyle} to={`/assets/create`}>
-                  <div className="content">
-                    <div className="ui left floated image" style={mascotStyle}>
-                      <img src="/images/mascots/shark.png" />
-                    </div>
-                    <h2 style={headerStyle}><i className="area chart icon" />&nbsp;Analytics</h2>
-                    <p style={descStyle}>Data beats opinions: analyze actual game usage and use it to improve.</p>
-                  </div>
-                </QLink>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+const cardStyle = {
+  color: "#2e2e2e"
 }
+
+const mascotStyle = {
+  maxWidth: "8em",
+  paddingRight: "0.5em",
+  marginBottom: "0"
+}
+
+const headerStyle = {
+  marginTop: "0.15em",
+  marginBottom: "0.4em"
+}
+
+const descStyle = {
+  fontSize: "1.25em",
+  lineHeight: "1.5em"
+}
+
+const skillAreaItems = [
+  { mascot: 'alien',    icon: 'code',        content: 'Programming',   desc: 'Code using JavaScript and game engines like Phaser' },
+  { mascot: 'penguin',  icon: 'paint brush', content: 'Pixel art',     desc: 'Create animated sprites, spritesheets and tilemaps for games.' },
+  { mascot: 'samurai',  icon: 'music',       content: 'Music & audio', desc: 'Bring engagement and mood to games through music and sound' },
+  { mascot: 'slimy',    icon: 'idea',        content: 'Game design',   desc: 'Design game levels, try different gameplay mechanics and balance your games' },
+  { mascot: 'vampire',  icon: 'write',       content: 'Story writing', desc: 'Bring game stories to life through plot, character, narrative and dialog' },
+  { mascot: 'shark',    icon: 'area chart',  content: 'Analytics',     desc: 'Data beats opinions: analyze actual game usage and use it to improve' } 
+]
+
+const GetStartedRoute = () => (
+  <Segment basic padded className="slim" style={{margin: '0 auto'}}>
+    <Grid stackable style={{marginTop: '3.5em'}} title={_notReallyWorkingYet}>
+
+      <Grid.Row >
+        <Grid.Column>
+          <Header as='h1' size='huge' style={{fontSize: '2.5em'}}>
+            What would you like to try?
+            <em className="sub header">There's a lot to learn</em>
+          </Header>
+        </Grid.Column>
+      </Grid.Row>
+
+      <Grid.Row>
+        <Grid.Column>
+          <Card.Group itemsPerRow={2} stackable className="skills">
+            { skillAreaItems.map( (area, idx) => (
+                <QLink key={idx} className="card" style={cardStyle} to={`/assets/create`}>
+                  <Card.Content>
+                    <Image floated='left' style={mascotStyle} src={`/images/mascots/${area.mascot}.png`} />
+                    <Header as='h2' style={headerStyle}><Icon name={area.icon} />&nbsp;{area.content}</Header>
+                    <p style={descStyle}>{area.desc}.</p>
+                  </Card.Content>
+                </QLink>
+              ))
+            }
+         </Card.Group>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Segment>
+)
+
+export default GetStartedRoute

e49427d02328c19f8ccf4c4f1e54cbdbf567982d
David Golds <david.p.golds@gmail.com>
2016-09-27 20:02:19 -0700

    Cleanup, shrink and reuse code for ImageShowOrChange

--- code13/app/client/imports/components/Controls/ImageShowOrChange.js
+++ code13/app/client/imports/components/Controls/ImageShowOrChange.js
@@ -3,68 +3,37 @@ import React, { PropTypes } from 'react'
 import DragNDropHelper from '/client/imports/helpers/DragNDropHelper'
 import QLink from '/client/imports/routes/QLink'
 
+const _getAssetIdFromUrl = url => url.startsWith("/api/asset/png") ? _.last(url.split("/")) : null
 
-function _getAssetIdFromUrl(url) {
-  return url.startsWith("/api/asset/png") ? _.last(url.split("/")) : null
+const _importFromDrop= (event, handleChange) => {
+  const asset = DragNDropHelper.getAssetFromEvent(event)
+  if (asset && asset.kind === 'graphic') {
+    const imgUrl = `/api/asset/png/${asset._id}`
+    handleChange(imgUrl, asset._id)
+  }
 }
 
-const _propTypes = {
+export default ImageShowOrChange = props => {
+  const { className, imageSrc, canEdit, canLinkToSrc, handleChange } = props
+  const avatarAssetId = _getAssetIdFromUrl(imageSrc)
+  
+  const propsImgContainer = {
+    title: canEdit && 'Drag an Image asset here to change the chosen image',
+    className: className, 
+    onDragOver: e => ( canEdit && DragNDropHelper.preventDefault(e) ),
+    onDrop: e => ( canEdit && _importFromDrop(e, handleChange) )
+  }
+  
+  if (canLinkToSrc) 
+    propsImgContainer.to = avatarAssetId ? `/assetEdit/${avatarAssetId}` : imageSrc
+
+  return React.createElement(canLinkToSrc ? QLink : 'div', propsImgContainer, <img className="ui fluid image mgb-pixelated" src={imageSrc} /> )
+}
+
+ImageShowOrChange._propTypes = {
   className:    PropTypes.string.isRequired,    // Classname for the outer div
   imageSrc:     PropTypes.string.isRequired,    // A string which will be passed to img.src
   canEdit:      PropTypes.bool.isRequired,      // True if this should be able to accept changes via Drag
   canLinkToSrc: PropTypes.bool.isRequired,      // True if this should be a QLink to the image (or image editor)
   handleChange: PropTypes.func.isRequired       // Function callback - takes (newUrlString, assetIdString) as params
 }
-
-export default class ImageShowOrChange extends React.Component {
-
-  render() {
-    const { className, imageSrc, canEdit, canLinkToSrc } = this.props
-    const avatarAssetId = _getAssetIdFromUrl(imageSrc)
-    const href = avatarAssetId ? `/assetEdit/${avatarAssetId}` : imageSrc
-    const img =  <img className="ui fluid image mgb-pixelated" src={imageSrc} />
-    const title = canEdit && "Drag an Image asset here to change the chosen image"
-    if (canLinkToSrc)
-      return (
-        <QLink to={ href }
-          title={ title }
-          className={className}           
-          onDragOver={(e) => this.prepareForDrag(e) }
-          onDrop={(e) => this.importFromDrop(e) } >
-         { img }
-        </QLink>
-      )
-    else 
-      return (
-        <div 
-          title={ title }
-          className={className}           
-          onDragOver={(e) => this.prepareForDrag(e) }
-          onDrop={(e) => this.importFromDrop(e) } >
-          { img }
-        </div>
-      )    
-  }
-
-  prepareForDrag(e) {
-    if (!this.props.canEdit)
-      return
-
-    e.stopPropagation()
-    e.preventDefault()
-    e.dataTransfer.effectAllowed = 'copy'
-  }
-
-  importFromDrop(e) {
-    if (!this.props.canEdit)
-      return
-
-    const asset = DragNDropHelper.getAssetFromEvent(e)
-    if (asset && asset.kind === 'graphic') {
-      const imgUrl = `/api/asset/png/${asset._id}`
-      this.props.handleChange(imgUrl, asset._id)
-    }
-  }
-}
-
-ImageShowOrChange._propTypes = _propTypes
--- code13/app/client/imports/helpers/DragNDropHelper.js
+++ code13/app/client/imports/helpers/DragNDropHelper.js
@@ -2,29 +2,27 @@
 
 const DragNDropHelper = {
   getAssetFromEvent: (e) => {
-    e.preventDefault();
-    const dataStr = e.dataTransfer.getData("text");
-    if(!dataStr){
-      return null;
-    }
-    let data;
-    try{
+    e.preventDefault()
+    const dataStr = e.dataTransfer.getData("text")
+    if (!dataStr)
+      return null
+    
+    let data
+    try {
       data = JSON.parse(dataStr);
     }
-    catch(e){
+    catch (e) {
       /* json failed to parse */
-      return null;
+      return null
     }
     return data.asset
   },
 
   preventDefault: (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    e.dataTransfer.effectAllowed = 'copy';
-    // IE crashes on this
-    // e.dataTransfer.dropEffect = 'copy';
+    e.stopPropagation()
+    e.preventDefault()
+    e.dataTransfer.effectAllowed = 'copy'
   }
-};
+}
 
-export default DragNDropHelper;
+export default DragNDropHelper

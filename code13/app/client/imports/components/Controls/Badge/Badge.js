6678e432bce403f2aa12624a1ae0db362677956b
David Golds <david.p.golds@gmail.com>
2016-09-30 11:31:46 -0700

    Update to use new badges. Some hacks in Badge.js for now

--- code13/app/client/imports/components/Controls/Badge/Badge.js
+++ code13/app/client/imports/components/Controls/Badge/Badge.js
@@ -1,12 +1,25 @@
+import _ from 'lodash'
 import React, { PropTypes } from 'react'
 import { isUserSuperAdmin } from '/imports/schemas/roles'
 
 const badgeList = {
-  mgb1veteran:      [ "Veteran Game Builder.png",   "MGB1 Veteran" ],
-  mgbAdmin:         [ "Metaclaw Admin.png",         "MGB Admin" ],
-  mgb2AlphaTester:  [ "MGB2 Alpha Tester.png",      "MGB2 Alpha Tester" ],
-  hasAvatar:        [ "Has Avatar.png",             "Has an Avatar!" ],
-  blank:            [ "Empty Badge slot.png",       "Space for another badge!" ]
+//mgb1veteran:      [ "v2/anvil.png",               'MGB1 Veteran' ],
+  mgbAdmin:         [ "v2/beginner/anvil.png",      'MGB Admin' ],
+//hasAvatar:        [ "Has Avatar.png",             'Has an Avatar' ],
+  mgb2AlphaTester:  [ "v2/beginner/bugfinder.png",  "Found a good MGB2 bug / is Alpha Tester" ],
+
+//hourOfMap:        [ 'v2/beginner/hourofmap.png',  'Hour+ of Mapping' ],
+//hourOfCode:       [ 'v2/beginner/hourofcode.png', 'Hour+ of Coding' ],
+  hourOfDrawing:    [ 'v2/beginner/hourofdrawing.png', 'Hour+ of Drawing' ],
+  hourOfSound:      [ 'v2/beginner/hourofsound.png', 'Hour+ of Sound Making' ],
+
+  tenHours  :      [ 'v2/beginner/tenhours.png',    'Ten Hours+ of Building' ],
+
+  guruCode:         [ 'v2/guru/code.png',           'Official MGB Code Guru'],
+  guruArt:          [ 'v2/guru/art.png',            'Official MGB Art Guru'],
+  guruMusic:        [ 'v2/guru/music.png',          'Official MGB Music Guru'],
+
+  blank:            [ "Empty Badge slot.png",       "Space for another badge" ]
 }
 
 const mgb2AlphUserCutoffDate = new Date("Dec 01 2016")
@@ -14,9 +27,26 @@ const mgb2AlphUserCutoffDate = new Date("Dec 01 2016")
 export const getImplicitBadgesForUser = function(user) {
   let retval = []
 
-  isUserSuperAdmin(user) && retval.push("mgbAdmin")
-  user.profile.mgb1name && user.profile.mgb1name.length > 0 && retval.push("mgb1veteran")
-  user.profile.avatar && user.profile.avatar.length > 0 && retval.push("hasAvatar")         // TODO: Fix this - it's wrong since we always do the gravatar hash
+// QUICK HACK TO TEST BADGES
+  if (_.includes('dgolds,stauzs,guntis'.split(','), user.profile.name))
+  {
+    retval.push('guruCode')
+    retval.push('hourOfDrawing')
+    retval.push('tenHours')
+  }
+
+  if (_.includes('dgolds,guntis'.split(','), user.profile.name))
+    retval.push('guruMusic')
+
+  if (_.includes('Micah'.split(','), user.profile.name))
+  {
+    retval.push('guruArt')
+    retval.push('hourOfDrawing')
+    retval.push('tenHours')
+  }
+  isUserSuperAdmin(user) && retval.push('mgbAdmin')
+//  user.profile.mgb1name && user.profile.mgb1name.length > 0 && retval.push("mgb1veteran")
+//  user.profile.avatar && user.profile.avatar.length > 0 && retval.push("hasAvatar")   // TODO: Fix this - it's wrong since we always do the gravatar hash
   user.createdAt < mgb2AlphUserCutoffDate && retval.push("mgb2AlphaTester")
 
   return retval
@@ -36,7 +66,7 @@ const Badge = props =>
   const imgUrl = "/images/badges/" + badge[0]
   const title = badge[1]
   const size = forceSize ? ({ width: `${forceSize}px`, height: `${forceSize}px`}) : ({})
-  return <img src={imgUrl} title={title} {...size} />
+  return <img style={{ maxWidth: '64px' }} src={imgUrl} title={title} {...size} />
 }
 
 Badge.propTypes = {

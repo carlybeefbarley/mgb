409d8ab9e408d63cf7d3bcd28a05e99396efd3c4
David Golds <david.p.golds@gmail.com>
2016-10-05 21:55:07 -0700

    update roadmap

--- code13/app/client/imports/routes/Roadmap.js
+++ code13/app/client/imports/routes/Roadmap.js
@@ -20,34 +20,30 @@ export default Roadmap = React.createClass({
   renderRoadmap: function() {
     return (
       <div>
-        <h2>Features planned for September 2016</h2>
+        <h2>Features planned for October 2016</h2>
         <ul>
-          <li>(DONE) Allow project members to edit assets in your projects</li>
-          <li>(DONE) Avatars for users and projects (drag graphic asset from FlexPanel)</li>
-          <li>(DONE) Multi-channel/track music editing/mixing</li>
-          <li>(IN PROGRESS) Code Editor code-assist features</li>
+          <li>(IN PROGRESS) Even more Code Editor code-assist features</li>
           <li>(IN PROGRESS) Tutorial and progression systems to help learn how to make games</li>
+          <li>(IN PROGRESS) A new 'Game' asset will provide a way to promote games and track plays</li>
           <li>MGBv1 asset migration (tiles, actors, maps)</li>
           <li>MGBv1-style 'Actor' assets which enable game building without coding</li>
           <li>Chat improvements (notifications, DMs etc)</li>
-          <li>A new 'Game' asset will provide a way to promote games and track plays</li>
         </ul>
         <h2>Features coming later in 2016</h2>
         <ul>
           <li>Asset Versioning/Publishing system</li>
           <li>Clone Asset / Clone Project</li>
-          <li>Easier Asset referencing from source code</li>
           <li>Email for password reset, notifications etc</li>
           <li>Game Discovery, Management and Analytics</li>
-          <li>Improved UI for touch-only devices like iPads</li>
+          <li>Further Improve UI for touch-only devices like iPads</li>
           <li>Keyboard shortcut editor and help</li>
           <li>Mentoring and AskForhelp systems</li>
-          <li>Project rename/delete</li>
+          <li>Project rename</li>
           <li>Test runner for MGB Code Assets</li>
-          <li>Tutorials and Tutorial editor</li>        
+          <li>Tutorials and Tutorial editor</li>
+          <li>Stock/sample assets system</li>
           <li>TODO tracker</li>
           <li>Skills tracker</li>
-          <li>Stock/sample assets system</li>
         </ul>
       </div>
     )

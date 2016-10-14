9e936f7e646d569c010c2f1c219235d14ecc4809
David Golds <david.p.golds@gmail.com>
2016-10-14 08:58:32 -0700

    Fix wall collision bugs

--- code13/app/client/imports/components/MapActorGameEngine/MageBlockageMap.js
+++ code13/app/client/imports/components/MapActorGameEngine/MageBlockageMap.js
@@ -4,47 +4,46 @@
 
 export default class BlockageMap
 {
-	reset(w, h)
-	{
-		this.cells = new Array(w * h)
-		this.width = w
-		this.height = h
-	}
+  reset(w, h)
+  {
+    this.cells = new Array(w * h)
+    this.width = w
+    this.height = h	
+  }
 
   constructor() {
 		// Does nothing. Caller should use reset() instead to prepare for use
-  }
-	
-  // static public consts...
-	ENTITY_PLAYER = 0
-	ENTITY_NPC = 1
-	
+  }	
 
   offsetforCell(x,y) 
   {
     if (x > this.width || y > this.height)
-			console.trace("Incorrect size in BlockageMap")
-		return y*this.width + x		// Arranged in rows  
-	}
+      console.trace("Incorrect size in BlockageMap")
+    return y*this.width + x		// Arranged in rows  
+  }
 	
-	blockEntity(x, y, entityIndex, w = 1, h = 1)
+  blockEntity(x, y, entityIndex, w = 1, h = 1)
 	{
-		for (var i = 0; i < w; i++) {
-			for (var j = 0; j < h; j++) {
-				if (x+i < this.width && y+j < this.height) {
-  				console.log("block "+(x+i)+","+(y+j)+" to "+entityIndex)
-					var c = this.offsetforCell(x+i, y+j)
-					var v = this.cells[c]
-					this.cells[c] = (v | (1 << entityIndex))
-				}
-			}
-		}
-	}
+    for (var i = 0; i < w; i++) {
+      for (var j = 0; j < h; j++) {
+        if (x+i < this.width && y+j < this.height) {
+          console.log("block "+(x+i)+","+(y+j)+" to "+entityIndex)
+          var c = this.offsetforCell(x+i, y+j)
+          var v = this.cells[c]
+          this.cells[c] = (v | (1 << entityIndex))
+        }
+      }
+    }
+  }
 	
-	isEntityBlocked(x, y, entityIndex)
+  isEntityBlocked(x, y, entityIndex)
 	{
-		var c = this.offsetforCell(x, y)
-		var v = this.cells[c]
-		return (v & (1 << entityIndex)) !== 0
-	} 
+    var c = this.offsetforCell(x, y)
+    var v = this.cells[c]
+    return (v & (1 << entityIndex)) !== 0
+  } 
 }
+
+// static public consts...
+BlockageMap.ENTITY_PLAYER = 0
+BlockageMap.ENTITY_NPC = 1

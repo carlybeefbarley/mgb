d6afd832df2ad0ce402caf2d0cb726face997962
David Golds <david.p.golds@gmail.com>
2016-10-17 14:45:28 -0700

    Fix animation tile selection. Need to fix effects next

--- code13/app/client/imports/components/MapActorGameEngine/MageActiveActorClass.js
+++ code13/app/client/imports/components/MapActorGameEngine/MageActiveActorClass.js
@@ -2,19 +2,19 @@
 
 export default class ActiveActor
 {
-	creationCause = ''						// CREATION_BY_MAP or CREATION_BY_SPAWN 
+	creationCause = ''				// CREATION_BY_MAP or CREATION_BY_SPAWN 
 	ACidx = ''								// Index of this actor in the actorCache cache - the index is the name
 	health = 0								// Current health
 	maxHealth = 0							// Current Maximum health
 	renderX = 0								// Render this at pixel X (not tile X)
 	renderY = 0								// Render this at pixel Y (not tile Y)
-	_image = null						// This shoudl be of type Image
+	_image = null							// This should be of type Image
 	cellSpanX = 0							// How many cells does this actor span (width)?
 	cellSpanY = 0							// How many cells does this actor span (height)?
 	
 	// Some caches of key actor properties - so we don't have to go into the content2 so often (for speed)
 	type = 0									// One of MgbActor.alActorType 
-	moveSpeed:Number							// From content2.databag.allchar.movementSpeedNum. However, also defined as 1 for a sliding block while isSliding = true
+	moveSpeed = 0							// From content2.databag.allchar.movementSpeedNum. However, also defined as 1 for a sliding block while isSliding = true
 
 	// Basic positioning info		
 	startx = 0			// Where this piece starts from. 	Units are in tiles, not pixels
@@ -50,31 +50,33 @@ export default class ActiveActor
 	isAShot = false							  // Only true for Shots
 	actorWhoFiredShot = 0					// Which actor fired this shot (metric valid for shots)
 	shotRange = 0							    // metric valid for Shots - comes from shooter's "content2.databag.allchar.shotRangeNum"
+
 	// Shot tracking - shooters
 	maxActiveShots = 0						// Maximum number of shots that can be active on map at once (metric valid for things that shoot)
 	currentActiveShots = 0			  // Current number of Active shots on map (metric valid for things that shoot)
 	shotDamageToNPC = 0
 	shotDamageToPlayer = 0
+
 	// Dead or dying (explosions etc)
 	alive = true							    // true if alive; false if dead or dying
 	dyingAnimationFrameCount = 0	// 0 mean no animation of dying; >0 means at frame N of that 'dying' animation
 	
 	// Active powers
-	activePowerUntilGetTime = 0			// 0 means no active power
-	activePower = 0							// One of the MgbActor.alGainPower powers. 
+	activePowerUntilGetTime = 0		// 0 means no active power
+	activePower = 0							  // One of the MgbActor.alGainPower powers. 
 
 	// Player-related info (this could be kept out of the class, but I've kept it here for simplicity and consistency, and it helps if we later support multi-user)
 	score = 0
-	extraLives = 0							// in addition to the one currently in use
+	extraLives = 0							  // in addition to the one currently in use
 	winLevel = false							// True when user reaches some special goal/marker etc
-	
-	// static consts
-	CREATION_BY_SPAWN = "spawned"
-	CREATION_BY_MAP = "mapped"
-	MELEESTEP_NOT_IN_MELEE = -1
 	
 	inMelee()
 	{
-		return this.meleeStep !== undefined && this.meleeStep != this.MELEESTEP_NOT_IN_MELEE
+		return this.meleeStep !== undefined && this.meleeStep != ActiveActor.MELEESTEP_NOT_IN_MELEE
 	}
 }
+
+// static consts
+ActiveActor.CREATION_BY_SPAWN = "spawned"
+ActiveActor.CREATION_BY_MAP = "mapped"
+ActiveActor.MELEESTEP_NOT_IN_MELEE = -1

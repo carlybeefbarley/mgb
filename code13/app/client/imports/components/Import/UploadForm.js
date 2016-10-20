570f0e1562acfaf681168cef19b1d6a9098240de
Guntis <guntis.smaukstelis@gmail.com>
2016-10-20 15:41:12 +0300

    error message for non-webkit browsers

--- code13/app/client/imports/components/Import/UploadForm.js
+++ code13/app/client/imports/components/Import/UploadForm.js
@@ -57,62 +57,75 @@ export default class UploadForm extends React.Component {
   }
 
   render (){
+    const isWebkit = 'WebkitAppearance' in document.documentElement.style
+    // console.log('isWebkit', isWebkit)
+    
     return (
       <div className={this.props.isHidden ? "hidden" : ""}> 
         
-        <div className="row" style={{padding:"10px 30px"}}>
+        <div className={"ui big negative message " + (isWebkit ? "hidden" : "")}>
+          <i className="close icon"></i>
+          <div className="header">
+            Your browser doesn't support bulk import!
+          </div>
+          <p>Please use webkit browsers like Chrome, Safari or Opera</p>
+        </div>
 
-          <div className="ui input mini focus">
-            Prefix &nbsp;
-            <input 
-              style={{padding:"2px 5px"}}
-              type="text"
-              value={this.state.prefix} 
-              onChange={this.handlePrefixChange.bind(this)} 
+        <div className={isWebkit ? "" : "hidden"}>
+          <div className="row" style={{padding:"10px 30px"}}>
+
+            <div className="ui input mini focus">
+              Prefix &nbsp;
+              <input 
+                style={{padding:"2px 5px"}}
+                type="text"
+                value={this.state.prefix} 
+                onChange={this.handlePrefixChange.bind(this)} 
+              />
+            </div>
+
+            &nbsp;&nbsp;
+            
+            <ProjectSelector
+              canEdit={false}
+              user={this.props.currUser}
+              handleChangeSelectedProjectName={this.handleChangeSelectedProjectName.bind(this)}
+              availableProjects={this.props.currUserProjects}
+              ProjectListLinkUrl={this.props.currUser && `/u/${this.props.currUser.profile.name}/projects`}
+              showProjectsUserIsMemberOf={true}
+              chosenProjectName={this.state.projectCompoundName} 
+            />
+
+            &nbsp;&nbsp;
+
+            <AssetLicense 
+              license={this.state.assetLicense} 
+              showMicro={true}
+              canEdit={true}
+              handleChange={this.handleLicenseChange.bind(this)}
+            />
+
+            &nbsp;&nbsp;
+
+            <WorkState 
+              workState={this.state.workState} 
+              popupPosition="bottom center"
+              showMicro={true}
+              canEdit={true}
+              handleChange={this.handleWorkStateChange.bind(this)}
             />
           </div>
 
-          &nbsp;&nbsp;
-          
-          <ProjectSelector
-            canEdit={false}
-            user={this.props.currUser}
-            handleChangeSelectedProjectName={this.handleChangeSelectedProjectName.bind(this)}
-            availableProjects={this.props.currUserProjects}
-            ProjectListLinkUrl={this.props.currUser && `/u/${this.props.currUser.profile.name}/projects`}
-            showProjectsUserIsMemberOf={true}
-            chosenProjectName={this.state.projectCompoundName} 
-          />
-
-          &nbsp;&nbsp;
-
-          <AssetLicense 
-            license={this.state.assetLicense} 
-            showMicro={true}
-            canEdit={true}
-            handleChange={this.handleLicenseChange.bind(this)}
-          />
-
-          &nbsp;&nbsp;
-
-          <WorkState 
-            workState={this.state.workState} 
-            popupPosition="bottom center"
-            showMicro={true}
-            canEdit={true}
-            handleChange={this.handleWorkStateChange.bind(this)}
-          />
-        </div>
-
-        {/* TODO inputs: projects, prefix, license, status */}
-        <div className={"importUploadForm " + (this.props.isDragOver ? "draggedOver" : "")}
-          onDragOver={this.onDragOver.bind(this)}
-          onDragLeave={this.onDragLeave.bind(this)}
-          onDrop={this.onDrop.bind(this)}>
-            <br/><br/><br/><br/><br/>
-            <h2>Drop folder here!</h2>
-            <p>You can drop folder with graphic assets like .jpg, .png</p>
-            <br/><br/><br/><br/><br/>
+          {/* TODO inputs: projects, prefix, license, status */}
+          <div className={"importUploadForm " + (this.props.isDragOver ? "draggedOver" : "")}
+            onDragOver={this.onDragOver.bind(this)}
+            onDragLeave={this.onDragLeave.bind(this)}
+            onDrop={this.onDrop.bind(this)}>
+              <br/><br/><br/><br/><br/>
+              <h2>Drop folder here!</h2>
+              <p>You can drop folder with graphic assets like .jpg, .png</p>
+              <br/><br/><br/><br/><br/>
+          </div>
         </div>
       </div>
     )

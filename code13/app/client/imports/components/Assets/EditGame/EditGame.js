f34f56b53912f4c9fbdd222f0dc312555a6ca76e
David Golds <david.p.golds@gmail.com>
2016-10-26 19:20:36 -0700

    WIP update for EditGame

--- code13/app/client/imports/components/Assets/EditGame/EditGame.js
+++ code13/app/client/imports/components/Assets/EditGame/EditGame.js
@@ -1,93 +1,83 @@
-
 import React, { PropTypes } from 'react'
-import { Segment, Header, Item, Checkbox } from 'semantic-ui-react'
-import ImageShowOrChange from '/client/imports/components/Controls/ImageShowOrChange'
+import BaseForm from '/client/imports/components/Controls/BaseForm.js'
+import { Button, Checkbox, Form, Input, Message, Radio, Select, TextArea } from 'semantic-ui-react'
 
-export default class EditGame extends React.Component 
-{
-  constructor(props) {
-    super(props)
-    this.state = {
-      startClientCodeAsset: '',
-      coverImage: ''
-      
-    }
+
+const gameTypes = {
+  'ActorMap Game (no code)': 'actorMapGame',
+  'Code-Based Game': "codeGame"
+}
+
+class EditGameForm extends BaseForm {
+  get data() {
+    return this.props.asset.content2 || { gameType: 'codeGame', startCode: '', startActorMap: ''}
   }
 
   render() {
-    const { startClientCodeAsset, coverImage } = this.state
-    const { asset, canEdit } = this.props
-    const isAssetNameValid = false
-
     return (
-      <div>
-        <Header as='h2' className="ui header">{asset.name}</Header>
-        <Segment padded>
-          <Header as='h4' className="ui header">Starting Code Asset</Header>
-          <Item.Group>
-            <div className={"ui fluid input" + (isAssetNameValid ? "" : " error")}>
-              <input 
-                  className="fluid" 
-                  type="text" 
-                  value={startClientCodeAsset} 
-                  onChange={(e) => this.setState({ startClientCodeAsset: e.target.value})}
-                  ref="inputAssetName">
-              </input>
-            </div>
-          </Item.Group>
-        </Segment>
+      <div className="ui form">
+        {this.dropArea("Cover Graphic", "coverGraphic", "graphic", null, asset => {
+          if (asset && asset.thumbnail)
+            this.props.handleSave(null, asset.thumbnail, "Updating thumbnail")
+        })}
+        { this.options("Game Type", 'gameType', gameTypes)}
+        { this.data.gameType === 'codeGame' && this.dropArea("Starting Code", 'startCode', 'code' )}
+        { this.data.gameType === 'actorMapGame' && this.dropArea("Starting ActorMap", 'startActorMap', 'actormap' )}
 
-        <Segment className="ui padded segment">
-          <Header as='h4' className="ui header">Game Cover Image</Header>
-            <QLink className="image" to={'/'} elOverride='div'>
-              <ImageShowOrChange
-                className="image"
-                imageSrc={coverImage}
-                canEdit={canEdit}
-                canLinkToSrc={canEdit}
-                handleChange={(newUrl, newId ) => console.log('TODO: CHANGE IMAGE' , newUrl, newId ) } />
-            </QLink>
-        </Segment>        
-
-        <Segment className="ui padded segment">
-          <Header as='h4' className="ui header">Screen Sizes</Header>
-          <Checkbox label='Smartphone (portrait)' />
-          <br />
-          <Checkbox label='Smartphone (landscape)' />
-          <br />
-          <Checkbox label='Full screen laptop/tablet' />
-          <br />
-        </Segment>
-
-        <Segment className="ui padded segment">
-          <Header as='h4' className="ui header">Works on...</Header>
-          <Checkbox label='Touch-only devices' />
-          <br />
-          <Checkbox label='Keyboard & Mouse devices' />
-          <br />
-          <Checkbox label='GamePad devices' />
-          <br />
-          <Checkbox label='Telepathic mind control' />
-        </Segment>
-
-        { false &&  // Future ideas
-        <Segment className="ui padded segment">
-          <Header as='h4' className="ui header">Features</Header>
-          <Checkbox label='Lobbies' />
-          <br />
-          <Checkbox label='Save Game state in cloud' />
-          <br />
-          <Checkbox label='Highscores' />
-          <br />
-          <Checkbox label='Achievements' />
-          <br />
-          <Checkbox label='Realtime MultiPlayer' />
-          <br />
-          <Checkbox label='Turn-based MultiPlayer' />
-          <br />
-        </Segment>
-      }
       </div>
     )
   }
 }
+
+export default class EditGame extends React.Component {
+
+  handleSave(reason, thumbnail) {
+    //console.log("Pretending... Saving....")
+    this.props.handleContentChange(this.props.asset.content2, thumbnail, reason)
+  }
+
+  render() {
+    return (
+      <EditGameForm asset={this.props.asset} onchange={this.handleSave.bind(this)}/>
+    )
+  }
+}
+
+// Future ideas
+        // <Segment className="ui padded segment">
+        //   <Header as='h4' className="ui header">Screen Sizes</Header>
+        //   <Checkbox label='Smartphone (portrait)' />
+        //   <br />
+        //   <Checkbox label='Smartphone (landscape)' />
+        //   <br />
+        //   <Checkbox label='Full screen laptop/tablet' />
+        //   <br />
+        // </Segment>
+
+        // <Segment className="ui padded segment">
+        //   <Header as='h4' className="ui header">Works on...</Header>
+        //   <Checkbox label='Touch-only devices' />
+        //   <br />
+        //   <Checkbox label='Keyboard & Mouse devices' />
+        //   <br />
+        //   <Checkbox label='GamePad devices' />
+        //   <br />
+        //   <Checkbox label='Telepathic mind control' />
+        // </Segment>
+
+        // { false &&  // Future ideas
+        // <Segment className="ui padded segment">
+        //   <Header as='h4' className="ui header">Features</Header>
+        //   <Checkbox label='Lobbies' />
+        //   <br />
+        //   <Checkbox label='Save Game state in cloud' />
+        //   <br />
+        //   <Checkbox label='Highscores' />
+        //   <br />
+        //   <Checkbox label='Achievements' />
+        //   <br />
+        //   <Checkbox label='Realtime MultiPlayer' />
+        //   <br />
+        //   <Checkbox label='Turn-based MultiPlayer' />
+        //   <br />
+        // </Segment>

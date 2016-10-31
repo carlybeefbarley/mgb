814a12042e15d30e585e31d6a3cc5f71efca8682
David Golds <david.p.golds@gmail.com>
2016-10-30 19:11:02 -0700

    fix warnings

--- code13/app/client/imports/components/Assets/AssetListSortBy.js
+++ code13/app/client/imports/components/Assets/AssetListSortBy.js
@@ -1,32 +1,31 @@
 import React, { PropTypes } from 'react'
 
-export default AssetListSortBy = props => {
-  const { chosenSortBy, handleChangeSortByClick } = props
-  const choiceOptions = "edited,name,kind".split(",")
+const _choiceOptions = "edited,name,kind".split(",")
 
-  return (
-    <div 
-        className="ui small simple dropdown item" 
-        style={{float: "right", color: "grey"}} 
-        title="Sort Assets By..">
-      { chosenSortBy }
-      <i className='dropdown icon' style={{marginLeft: '0.2em'}}/>
-      <div className="ui small fitted menu">
-        { choiceOptions.map((k) => (
-            <a  className={"ui item"+ (k === chosenSortBy ? " active" : "")} 
-                data-value={k} key={k} 
-                onClick={ () => handleChangeSortByClick(k) }>
-              {k}
-            </a>
-          ))
-        }
-      </div>
-      &emsp;
+const AssetListSortBy = ( { chosenSortBy, handleChangeSortByClick } ) => (
+  <div 
+      className="ui small simple dropdown item" 
+      style={{float: "right", color: "grey"}} 
+      title="Sort Assets By..">
+    { chosenSortBy }
+    <i className='dropdown icon' style={{marginLeft: '0.2em'}}/>
+    <div className="ui small fitted menu">
+      { _choiceOptions.map((k) => (
+          <a  className={"ui item"+ (k === chosenSortBy ? " active" : "")} 
+              data-value={k} key={k} 
+              onClick={ () => handleChangeSortByClick(k) }>
+            {k}
+          </a>
+        ))
+      }
     </div>
-  )
-}
+    &emsp;
+  </div>
+)
 
 AssetListSortBy.propTypes = {
   chosenSortBy: PropTypes.string,
   handleChangeSortByClick: PropTypes.func.isRequired
 }
+
+export default AssetListSortBy

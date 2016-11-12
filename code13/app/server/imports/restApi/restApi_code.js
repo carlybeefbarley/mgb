4212d0b5198f68abff0cb6dd975e1867151cd7b8
David Golds <david.p.golds@gmail.com>
2016-11-11 16:29:30 -0800

    More helpful need-bundle-build message

--- code13/app/server/imports/restApi/restApi_code.js
+++ code13/app/server/imports/restApi/restApi_code.js
@@ -86,7 +86,8 @@ const _makeBundle = asset => {
   const { bundle } = asset.content2
   // reload page after 1 second - as bundle may be on the way to the server
   const extraMessage = !!bundle ? '' : `
-No bundle yet. Please run the source file in the code to generate/refresh the bundle
+<h2>The distribution bundle for this game must be created</h2>
+You can then run/rebuild the source file in the <a href='/u/${asset.dn_ownerName}/asset/${asset._id}'>Code Editor</a> to generate the bundle
 <script>window.setTimeout(function(){window.location.reload()}, 1000)</script>
 `
     

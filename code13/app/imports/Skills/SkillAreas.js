956bc527239cb9a4ef1a0703ec6f0df55b8256e8
David Golds <david.p.golds@gmail.com>
2016-11-30 22:39:22 -0800

    more text tweaks

--- code13/app/imports/Skills/SkillAreas.js
+++ code13/app/imports/Skills/SkillAreas.js
@@ -8,11 +8,11 @@ export const skillAreaItems = [
   { tag: 'code',       mascot: 'bigguy',     icon: 'code',        title: 'Programming',   desc: 'Code using JavaScript and game engines like Phaser' },
   { tag: 'art',        mascot: 'penguin',    icon: 'paint brush', title: 'Pixel art',     desc: 'Create animated sprites, spritesheets and tilemaps for games' },
   { tag: 'audio',      mascot: 'flyingcat',  icon: 'music',       title: 'Music & audio', desc: 'Bring engagement and mood to games through music and sound' },
-  { tag: 'design',     mascot: 'ideaguy',    icon: 'idea',        title: 'Game design',   desc: 'Design levels, balance gameplay mechanics and try new ones' },
+  { tag: 'design',     mascot: 'ideaguy',    icon: 'idea',        title: 'Game design',   desc: 'Design levels, balance gameplay mechanics and keep it fun' },
   { tag: 'writing',    mascot: 'duck',       icon: 'book',        title: 'Story writing', desc: 'Bring game stories to life through plot, character, narrative and dialog' },
-  { tag: 'analytics',  mascot: 'whale',      icon: 'line chart',  title: 'Analytics',     desc: 'Data beats opinions: analyze actual game usage and use it to improve' },
+  { tag: 'analytics',  mascot: 'whale',      icon: 'line chart',  title: 'Analytics',     desc: 'Data beats opinions: analyze player activity and improve all the things' },
   { tag: 'community',  mascot: 'slimy2',     icon: 'umbrella',    title: 'Community',     desc: 'Learn to organize, communicate and playtest with player communities' },
   { tag: 'marketing',  mascot: 'game_runner',icon: 'flag outline',title: 'Marketing',     desc: 'Learn to publicise your game and get people to play it' },
-  { tag: 'legal',      mascot: 'rpgGuy',     icon: 'law',         title: 'Legal',         desc: 'Protect your ideas and work. Stay out of jail also. ' },
+  { tag: 'legal',      mascot: 'rpgGuy',     icon: 'law',         title: 'Legal',         desc: 'Learn to protect your work and stay out of jail' },
   { tag: 'business',   mascot: 'game_shop',  icon: 'dollar',      title: 'Business',      desc: 'Learn the best ways to make a good profit from your games' }
 ]

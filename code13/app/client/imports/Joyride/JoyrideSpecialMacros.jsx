075089c9b725c70ef6d6acf8c50ef9260b9ca335
David Golds <david.p.golds@gmail.com>
2016-12-17 21:52:28 -0800

    Implement more tutorial macros and helpers

--- code13/app/client/imports/Joyride/JoyrideSpecialMacros.jsx
+++ code13/app/client/imports/Joyride/JoyrideSpecialMacros.jsx
@@ -54,7 +54,26 @@ const _mkFp = ( fpname, icon ) => (
   }
 )
 
+const _mkCreateAsset = kind => (
+  {
+    field: _fullStepField,
+    key: _wrapKey(`create-asset-${kind}`),
+    desc: `Step for awaiting creation of a ${_.upperCase(kind)} Asset`,
+    newVal:
+    {
+      "title": `Create a ${_.upperCase(kind)} Asset`,
+      "text": `First, type in a name for the asset above... 
+      for example '<span style='color: red'>my avatar</span>'.<br></br>Second.. Select the ${_.upperCase(kind)} asset kind above.<br></br>Third... Click on the 'Create Asset' button to the left here`,
+      "selector": "#mgbjr-create-asset-button",
+      "awaitCompletionTag": `mgbjr-CT-asset-create-new-${kind}`,
+      "position": "right"
+    }
+  }
+)
+
 const macros = [
+
+  // field == someFieldName means this is a macros for fields WITHIN a step.. e.g ."style"
   {
     field: 'style',
     key: _wrapKey('inverted'),
@@ -64,13 +83,54 @@ const macros = [
       "backgroundColor": "rgba(0, 0, 0, 0.8)",
       "color": "#fff",
       "mainColor": "#ff4456",
-      "skip": { "color": "#f04" },
+      "skip": { "color": "#ff4456" },
       "hole": { "backgroundColor": "RGBA(201, 23, 33, 0.2)" }
     }
   },
 
-  // Field == null means this is to replace an entire step
+  {
+    field: 'style',
+    key: _wrapKey('green'),
+    desc: "An very green style for success",
+    newVal:
+    {
+      "backgroundColor": "rgba(0, 96, 0, 1)",
+      "color": "#fff",
+      "mainColor": "#fbbd08",
+      "skip": { "color": "#f04" },
+      "hole": { "backgroundColor": "RGBA(201, 23, 33, 0.2)" }
+    }
+  },
+  // field == null means this is to replace an entire step  
 
+  {
+    field: _fullStepField,
+    key: _wrapKey('complete'),
+    desc: `Tutorial completed`,
+    newVal:
+    {
+      "title": `Great! You completed the tutorial`,
+      "text": `You can start the next tutorial from the Goals FlexPanel at any time`,
+      "selector": "#mgbjr-flexPanelIcons-goals",
+      "position": "left",
+      "style": "%green%"    // Note that full Step Macros can still use per-field macros :)
+    }
+  },
+
+  {
+    field: _fullStepField,
+    key: _wrapKey('MOCK'),
+    desc: `A Mock step that is handy as a placeholder when making a tutorial`,
+    newVal:
+    {
+      "title": `MOCK STEP. Placeholder - Does nothing`,
+      "text": `Lorem ipsum hocus pocus testing 1 2 3 Ground Control To Major Tom. Kthxbye`,
+      "selector": "body",
+      "position": "top",
+      "style": "%inverted%"    // Note that full Step Macros can still use per-field macros :)
+    }
+  },
+  
   {
     field: _fullStepField,
     key: _wrapKey('flexPanel'),
@@ -101,6 +161,20 @@ const macros = [
     }
   },
 
+  {
+    field: _fullStepField,
+    key: _wrapKey('create-new-asset'),
+    desc: `Step for Create New Asset. Prior step should be %np-create%`,
+    newVal:
+    {
+      "title": "Great. Now choose 'Create New Asset'",
+      "text": "Click on the&ensp;<div class='ui label'>Create New Asset&emsp;<i class='green pencil icon'></i></div>&ensp;option",
+      "selector": "#mgbjr-np-create-createNewAsset",
+      "awaitCompletionTag": "mgbjr-CT-app-location-path-/assets/create",
+      "position": "right"
+    },
+  },
+
   _mkNp( 'home',     'home'         ),
   _mkNp( 'learn',    'student'      ),
   _mkNp( 'create',   'pencil'       ),
@@ -111,13 +185,24 @@ const macros = [
 
   _mkFp( 'activity', 'lightning'    ),
   _mkFp( 'goals',    'student'      ),
-  _mkFp( 'asssets',  'pencil'       ),
+  _mkFp( 'assets',   'pencil'       ),
   _mkFp( 'chat',     'chat'         ),
   _mkFp( 'options',  'options'      ),
   _mkFp( 'skills',   'plus circle'  ),
   _mkFp( 'users',    'street view'  ),
   _mkFp( 'network',  'signal'       ),
-  _mkFp( 'keys',     'keyboard'     )
+  _mkFp( 'keys',     'keyboard'     ),
+
+  _mkCreateAsset( 'graphic'  ),
+  _mkCreateAsset( 'actor'    ),
+  _mkCreateAsset( 'actorMap' ),
+  _mkCreateAsset( 'map'      ),
+  _mkCreateAsset( 'code'     ),
+  _mkCreateAsset( 'sound'    ),
+  _mkCreateAsset( 'music'    ),
+  _mkCreateAsset( 'game'     ),
+  _mkCreateAsset( 'tutorial' )
+
 ]
 
 // This returns { newStep{}, notFoundMacros[] }  .. and never returns null, nor a different shape
@@ -163,5 +248,72 @@ export const transformStep = step =>
     }
     console.log(transformStep(s1))
     console.log(transformStep('%npHome%'))
+
+*/
+
+
+/* Example Tutorial JSON
+
+  {
+    "steps": [
+      {
+        "heading": "Setting An Avatar",
+        "title": "What's an Avatar?",
+        "text": "Your Avatar is an image that you choose to represent you on this site.<br></br>Let's learn how to set your user Avatar",
+        "style": "%inverted%",
+        "imageRightSrc": "/api/asset/png/!vault/AccountAvatar"
+      },
+
+      {
+        "title": "Avatars are Graphic Assets",
+        "text": "You must first create a graphic Asset that will be used for your Avatar"
+      },
+
+      "%np-create%",
+      "%create-new-asset%",
+      "%create-asset-graphic%",
+
+      {
+        "title": "This is your new, empty GRAPHIC Asset",
+        "text": "Use your mouse/finger to draw in the drawing area on the left. Your changes will save within a few seconds.<br></br>Don't worry about how it looks - you can easily improve it later.<br></br>Click Next here when ready",
+        "position": "top-right",
+        "imageRightSrc": "/api/asset/png/dgolds/foobar",
+        "TODO": "Put the image in !vault"
+      },
+
+      {
+        "title": "To the Profile Page!",
+        "text": "Let's go back to your profile page"
+      },
+
+      "%np-home%",
+
+      {
+        "title": "My Profile",
+        "text": "Click on the &ensp;<div class='ui label'>My Profile&emsp;<i class='grey user icon'></i></div>&ensp; option",
+        "selector": "#mgbjr-np-home-myProfile",
+        "awaitCompletionTag": "mgbjr-CT-app-router-path-u/:username",
+        "position": "right"
+      },
+
+      {
+        "title": "One more step...",
+        "text": "Next we are going to drag your graphic to your Profile Avatar"
+      },
+
+      "%flexPanel%",
+      "%fp-assets%",
+      {
+        "title": "Drag your graphic to set your avatar",
+        "text": "Find your new graphic in the Assets Flex Panel and drag it to the avatar area",
+        "awaitCompletionTag": "mgbjr-CT-profile-set-field-profile.avatar",
+        "position": "bottom",
+        "style": "%inverted%"
+      },
+
+      "%complete%"
+    ]
+  }
+
 
 */
--- code13/app/client/imports/components/Assets/EditCode/TutorialEditHelpers.js
+++ code13/app/client/imports/components/Assets/EditCode/TutorialEditHelpers.js
@@ -1,26 +1,27 @@
 import _ from 'lodash' 
 import React, { PropTypes } from 'react'
-import { Segment, Message } from 'semantic-ui-react'
+import { Header, Button, Segment, Message, Icon, List } from 'semantic-ui-react'
 import { parseStepsWithMacroResults } from '/client/imports/Joyride/Joyride'
 
-const blueSty = { color: 'blue' }
-const redSty =  { color: 'red' }
+const isMacroStepSty = { color: 'green' }
+const errorSty =       { color: 'red'   }
+const hasTodoSty =     { color: 'blue'  }
 
 const StepSummary = ( { rawStep, parsedStep, stepErrors } ) => 
 {
-  // <li key={idx}>{s.title || '(Missing title: field in step)'}</li>
   return (
-    <li >
-      { _.isString(rawStep) && <span style={blueSty}>{rawStep} </span>}
-      {rawStep.title || parsedStep.title || (_.isString(parsedStep) ? <span style={redSty}>Unknown Macro</span> : '(Missing title: field in step)')}
+    <List.Item >
+      { _.isString(rawStep) && <span style={isMacroStepSty}>[{rawStep}]&nbsp;</span>}
+      { _.has(parsedStep, 'TODO') && <small style={hasTodoSty}>[TODO: {parsedStep.TODO}] <br /></small> }
+      { rawStep.title || parsedStep.title || (_.isString(parsedStep) ? <span style={errorSty}>Unknown Macro</span> : '(Missing title: field in step)')}
       { (stepErrors && stepErrors.length > 0) ?
-        <ul>
-          { _.map(stepErrors, (e, idx) => <pre key={idx} style={redSty}>{e.key} : {e.val}</pre> ) }
-        </ul>
+        <List.List>
+          { _.map(stepErrors, (e, idx) => <List.Item key={idx} style={errorSty}>Error: "{e.key}" : {e.val}</List.Item> ) }
+        </List.List>
         :
         <br />
       }
-    </li>
+    </List.Item>
   )
 }
 
@@ -28,43 +29,47 @@ const TutSummary = props => {
   const pj = props.parsedTutorialData
 
   if (pj.errorHintString)
-    return <Message warning content={`JSON Parse error: ${pj.errorHintString}`} />
+    return <Message error icon='warning sign' header='JSON Parse error:' content={pj.errorHintString} />
 
   if (!pj.data)
-    return <Message info content='Empty Tutorial'/>
+    return <Message info header='Empty Tutorial' content='If you delete all content you can choose a starter template'/>
 
   if (!_.isArray(pj.data.steps))
-    return <Message warning content='No steps: [] array defined'/>
+    return <Message warning header='No "steps:" array defined' content='You must define a steps [] array for the tutorial'/>
 
   if (pj.data.steps.length === 0)
-    return <Message warning content='steps: [] array must have at least one step'/>
+    return <Message warning header='No "steps:" array content' content='The "steps:" array must have at least one step'/>
 
   const steps2 = parseStepsWithMacroResults(pj.data.steps)
 
   return (
     <Segment basic>
-      <Message success content='JSON Parses OK' /> 
-      Tutorial has { pj.data.steps.length } steps:
-      <ol>
-      { _.map(pj.data.steps, (s,idx) => <StepSummary key={idx} rawStep={s} parsedStep={steps2.newSteps[idx]} stepErrors={steps2.errors[idx]} />) }
-      </ol>
+      <Message success compact icon='checkmark box' header='JSON Parses OK' content={`Tutorial has ${pj.data.steps.length} steps`}/> 
+      <Header as='h4'>Tutorial steps:</Header>
+      <List ordered>
+        { _.map(pj.data.steps, (s,idx) => <StepSummary key={idx} rawStep={s} parsedStep={steps2.newSteps[idx]} stepErrors={steps2.errors[idx]} />) }
+      </List>
     </Segment>
   )
 }
 
 
-const TutorialMentor = ( { tryTutorial, stopTutorial, parsedTutorialData } ) => (
-  <div>
-    <button className='ui small yellow button' onClick={tryTutorial}>
-      <i className='student icon' />Try Tutorial
-    </button>
-    <button className='ui small yellow button' onClick={stopTutorial}>
-      <i className='stop icon' />Stop Tutorial
-    </button>
-    <br />
-    <TutSummary parsedTutorialData={parsedTutorialData} />
-  </div>
-)
+const TutorialMentor = ( { tryTutorial, stopTutorial, parsedTutorialData } ) => { 
+  const pj = parsedTutorialData
+  const badData = (pj.errorHintString || !pj.data || !_.isArray(pj.data.steps) || pj.data.steps.length === 0)
+  return (
+    <div>
+      <Button size='small' color='yellow' disabled={badData} onClick={tryTutorial}>
+        <Icon name='student' />Try Tutorial
+      </Button>
+      <Button size='small' color='yellow' onClick={stopTutorial}>
+        <Icon name='stop' />Stop Tutorial
+      </Button>
+      <br />
+      <TutSummary parsedTutorialData={parsedTutorialData} />
+    </div>
+  )
+}
 
 TutorialMentor.propTypes = {
   parsedTutorialData: PropTypes.object,   // null for not valid, or an object set by TutorialMentor.parseJson

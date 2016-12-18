3f691c11b1ff5c7cfa8bad152f1418fca3421a6c
David Golds <david.p.golds@gmail.com>
2016-12-18 13:07:14 -0800

    tutorial templates now use macros

--- code13/app/client/imports/components/Assets/EditCode/templates/TemplateTutorial.js
+++ code13/app/client/imports/components/Assets/EditCode/templates/TemplateTutorial.js
@@ -2,35 +2,22 @@ export const templateTutorial = [
   
   { label: "Simple Tutorial",
     description: "Simple Tutorial with just 4 basic steps",
-    code: `{
-  "steps": [{
-    	"heading": "Your Profile Page",
+    code: `{{
+  "steps": [
+    {
+      "heading": "Your Profile Page",
       "title": "Profile Page",
-      "text": "Let's find your profile page",
-      "selector": "body",
-      "position": "top",
-      "showStepOverlay": false
-    }, {
-      "title": "Navigation panel",
-      "text": "Click on the HOME button here",
-      "selector": "#mgbjr-navPanelIcons-home",
-      "showStepOverlay": false,
-      "awaitCompletionTag": "mgbjr-CT-navPanel-home-show",
-      "position": "right"
-    }, {
-      "title": "My Profile",
-      "text": "Click on the 'My Profile' button",
-      "selector": "#mgbjr-np-home-myProfile",
-      "showStepOverlay": false,
-      "awaitCompletionTag": "mgbjr-CT-app-router-path-u/:username",
-      "position": "right"
-    }, {
+      "text": "Let's find your profile page"
+    },
+
+    "%np-home%",
+    "%np-home-myProfile%",
+
+    {
       "title": "Well done!",
-      "text": "You can see it now...",
-      "selector": "body",
-      "position": "top",
-      "showStepOverlay": false
-    }
+      "text": "You can see it now..."
+    },
+    "%complete%"
   ]
 }`
   }

d6c8fab14cd658676a86b481fb92580d06575240
Kaspars Stauzs <stauzs@gmail.com>
2017-01-21 16:05:25 +0200

    http(s) fix

--- code13/app/server/cloudfront/CreateCloudfront.js
+++ code13/app/server/cloudfront/CreateCloudfront.js
@@ -231,12 +231,12 @@ export const setUpCloudfront = function () {
     if(req.headers.origin){
       const domain = req.headers.origin.split("//").pop()
       const origin = allowedOrigins.find( o => o.startsWith(domain))
-      if (index > -1) {
+      if (origin) {
         res.setHeader('access-control-allow-origin', origin)
     }
     // or allow for all domains
     // res.setHeader('access-control-allow-origin', '*')
-      
+
     }
     res.setHeader('access-control-expose-headers', 'etag')
 

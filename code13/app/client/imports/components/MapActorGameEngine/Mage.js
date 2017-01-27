ddcd0566ebce9b8cd81a1c08b61dbb72b18b5570
David Golds <david.p.golds@gmail.com>
2017-01-27 15:02:59 -0800

    fix icon warning

--- code13/app/client/imports/components/MapActorGameEngine/Mage.js
+++ code13/app/client/imports/components/MapActorGameEngine/Mage.js
@@ -8,7 +8,7 @@ import MageNpcDialog from './MageNpcDialog'
 import MageGameCanvas from './MageGameCanvas'
 import MageInventoryDialog from './MageInventoryDialog'
 
-import { Message, Button } from 'semantic-ui-react'
+import { Message, Button, Icon } from 'semantic-ui-react'
 
 import TouchController from './TouchController'
 
@@ -42,7 +42,7 @@ let _haveShownInstructionsOnceSinceStart = false      // We show this once per a
 
 
 const _compactMsgSty = { maxWidth: '500px' }  // Message icon cancels compact prop, so need a style
-const Preloader = ( { msg } ) => <Message style={_compactMsgSty} icon='circle notched loading' content={`Preloading ${msg}`} />
+const Preloader = ( { msg } ) => <Message style={_compactMsgSty} icon={<Icon name='circle notched' loading/>} content={`Preloading ${msg}`} />
 const MapLoadFailed = ( { err } ) => <Message style={_compactMsgSty} icon='warning sign' error content={err} />
 
 const _resolveOwner = (implicitOwnerName, assetName) => {

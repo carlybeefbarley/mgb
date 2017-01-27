0adf5f062a877c4598560ade462c06c475a78feb
Levi Thomason <me@levithomason.com>
2017-01-27 12:18:41 -0800

    feat(NavPanelItem): control open state

--- code13/app/client/imports/components/SidePanels/NavPanelItem.js
+++ code13/app/client/imports/components/SidePanels/NavPanelItem.js
@@ -1,33 +1,54 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import QLink from '/client/imports/routes/QLink'
 import { Dropdown } from 'semantic-ui-react'
 import _ from 'lodash'
 
-const NavPanelItem = ({ hdr, menu, style, to }) => (
-  <Dropdown item simple trigger={<QLink to={to}>{hdr}</QLink>} icon={null} style={style}>
-    <Dropdown.Menu>
-      {_.map(menu, ({ subcomponent, ...subcomponentProps }) => {
-        return React.createElement(Dropdown[subcomponent], {
-          key: subcomponentProps.content,
-          as: QLink,
-          ...subcomponentProps,
-        })
-      })}
-    </Dropdown.Menu>
-  </Dropdown>
-)
+class NavPanelItem extends Component {
+  static propTypes = {
+    hdr: PropTypes.node,
+    menu: PropTypes.arrayOf(PropTypes.shape({
+      subcomponent: PropTypes.oneOf(['Item', 'Header', 'Divider']),
+      to: PropTypes.string,
+      icon: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.object,
+      ]),
+      content: PropTypes.oneOfType([PropTypes.string, PropTypes.node])
+    })),
+  }
 
-NavPanelItem.propTypes = {
-  hdr: PropTypes.node,
-  menu: PropTypes.arrayOf(PropTypes.shape({
-    subcomponent: PropTypes.oneOf(['Item', 'Header', 'Divider']),
-    to: PropTypes.string,
-    icon: PropTypes.oneOfType([
-      PropTypes.node,
-      PropTypes.object,
-    ]),
-    content: PropTypes.oneOfType([PropTypes.string,PropTypes.node])
-  })),
+  state = { open: false }
+
+  handleMouseEnter = () => this.setState({ open: true })
+  handleMouseLeave = () => this.setState({ open: false })
+  handleItemClick = () => this.setState({ open: false })
+
+  render() {
+    const { hdr, menu, style, to } = this.props
+    const { open } = this.state
+
+    return (
+      <Dropdown
+        item
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        trigger={<QLink to={to}>{hdr}</QLink>}
+        icon={null}
+        open={open}
+        style={style}>
+        <Dropdown.Menu>
+          {_.map(menu, ({ subcomponent, ...subcomponentProps }) => {
+            return React.createElement(Dropdown[subcomponent], {
+              key: subcomponentProps.content,
+              as: QLink,
+              onClick: this.handleItemClick,
+              ...subcomponentProps,
+            })
+          })}
+        </Dropdown.Menu>
+      </Dropdown>
+    )
+  }
 }
 
 export default NavPanelItem

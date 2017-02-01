37a05b44efb1b9226ee74663295605b5a9883e58
Guntis <guntis.smaukstelis@gmail.com>
2017-02-01 17:04:08 +0200

    supported file types for audio editors

--- code13/app/client/imports/components/Assets/EditAudio/EditSound/ImportSound.js
+++ code13/app/client/imports/components/Assets/EditAudio/EditSound/ImportSound.js
@@ -16,6 +16,16 @@ export default class ImportSound extends React.Component {
 	    status: "empty" // empty, draggedOver, uploading, uploaded
 	    , playerStatus: "empty" // empty, play, pause
 	  }
+
+    const fileFormats = ['mp3', 'wav', 'ogg', 'mp4', 'mpeg']
+    this.supportedFormats = []
+    const audio = new Audio()
+    fileFormats.forEach( format => {
+      if(audio.canPlayType('audio/'+format)){
+        this.supportedFormats.push(format)
+      }
+    })
+    // console.log(this.supportedFormats)
 	}
 
 	componentDidMount(){
@@ -152,6 +162,7 @@ export default class ImportSound extends React.Component {
       		onDrop={this.onDrop.bind(this)}>
       			<br/><br/><br/><br/><br/>
       			<h2>Drop sound file here!</h2>
+            <p>Supported formats: {this.supportedFormats.join(", ")}</p>
       			<br/><br/><br/><br/><br/>
       	</div>
 

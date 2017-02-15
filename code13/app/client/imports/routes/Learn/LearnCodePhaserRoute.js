187f798f4acb6e481f0344f0a9017a5507d3c47f
Guntis <guntis.smaukstelis@gmail.com>
2017-02-15 19:45:11 +0200

    phaser tutorials redirects to code via tutorial system

--- code13/app/client/imports/routes/Learn/LearnCodePhaserRoute.js
+++ code13/app/client/imports/routes/Learn/LearnCodePhaserRoute.js
@@ -7,6 +7,9 @@ import { Segment, Grid, Card, Header } from 'semantic-ui-react'
 import SkillNodes from '/imports/Skills/SkillNodes/SkillNodes'
 import SkillsMap from '/client/imports/components/Skills/SkillsMap.js'
 
+import { startSkillPathTutorial } from '/client/imports/routes/App'
+
+
 
 const phaserSkills = SkillNodes.code.js.phaser
 const skillItems = []
@@ -43,6 +46,10 @@ const LearnCodePhaserRoute = ( { currUser, params }, context ) => {
       
       <Grid stackable>
         <Grid.Row >
+          
+          {/*<div onClick={() => startSkillPathTutorial('getStarted.nonCodeGame.createGraphic') }>Start code tutorial</div>*/}
+
+
           <Header as='h1' size='huge' style={{fontSize: '2.5em'}}>
             Game development concepts
             <em className="sub header">These concept examples use the 'Phaser' game engine for JavaScript. However, the concepts you learn here are very important and will apply to any game engine you use in future.</em>
@@ -51,25 +58,28 @@ const LearnCodePhaserRoute = ( { currUser, params }, context ) => {
         <Grid.Row>
           <Card.Group itemsPerRow={1} stackable className="skills">
             { skillItems.map( (area, idx) => {
-              const key = 'code/js/phaser/'+area.idx+'/'+area.idx
+              let skillPath = 'code/js/phaser/'+area.idx+'/'+area.idx
               const cStyle = _.cloneDeep(cardStyle)
 
-              if(currUser && context.skills[key]){
+              if(currUser && context.skills[skillPath]){
                 cStyle.backgroundColor = "#e2fcd6"
                 // console.log(area.idx)
               }
 
               return (
-              <QLink key={idx} to={area.link} className='card animated fadeIn' style={cStyle} to={area.link}>
-                <Card.Content>
+                <div  key={idx}  style={cStyle}  className='card animated fadeIn'>
+                <Card.Content onClick={ () => startSkillPathTutorial(skillPath) }>
                   <p style={descStyle}>
                     <i className={area.icon+" large icon"}></i>
                     <b>{area.name}</b>
                     &nbsp;- {area.description}
                   </p>
                 </Card.Content>
-              </QLink>
-              )})
+                </div>
+              )
+              
+              
+              })
             }
 
           </Card.Group>
@@ -87,7 +97,8 @@ export default LearnCodePhaserRoute
 // styles
 
 const cardStyle = {
-  color: "#2e2e2e"
+  color: "#2e2e2e",
+  cursor: "pointer"
 }
 
 const descStyle = {

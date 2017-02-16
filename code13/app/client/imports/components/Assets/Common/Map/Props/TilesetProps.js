912398cc41bd3a5ed14129d0faadeba606df9f2b
Kaspars Stauzs <stauzs@gmail.com>
2017-02-16 21:57:33 +0200

    Maps: call tilecache updates one by one: fix: #382 and maybe #390

--- code13/app/client/imports/components/Assets/Common/Map/Props/TilesetProps.js
+++ code13/app/client/imports/components/Assets/Common/Map/Props/TilesetProps.js
@@ -39,15 +39,29 @@ export default {
     const reason = "Removed Tileset: " + c2.tilesets[id].name
 
     this.saveForUndo(reason)
+    this.setState({activeTileset: this.state.activeTileset > 0 ? this.state.activeTileset -1 : 0})
 
     c2.tilesets.splice(id, 1)
-    this.cache.update(c2)
+    // update cache - so we can zero out tiles
+    this.cache.update(c2, () => {
+      // async call - map can be unloaded already
+      if(!this.cache)
+        return
 
-    this.state.activeTileset = this.state.activeTileset > 0 ? this.state.activeTileset -1 : 0
-    TileHelper.zeroOutUnreachableTiles(c2, this.cache.tiles)
+      TileHelper.zeroOutUnreachableTiles(c2, this.cache.tiles)
 
-    this.quickSave(reason)
-    this.setState({activeTileset: this.state.activeTileset})
+      this.cache.update(c2, () => {
+        // async call map can be removed already
+        if(!this.cache)
+          return
+        this.quickSave(reason)
+        this.setState({activeTileset: this.state.activeTileset})
+      })
+    })
+
+
+
+
   },
   selectTileset: function(id){
     this.setState({activeTileset: id})

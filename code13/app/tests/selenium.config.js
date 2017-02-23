97a6e19d72d98d53c0752dd9567f00d35ac75fed
Kaspars Stauzs <stauzs@gmail.com>
2017-02-23 12:14:45 +0200

    Added phantom browser

--- code13/app/tests/selenium.config.js
+++ code13/app/tests/selenium.config.js
@@ -1,11 +1,12 @@
 // config in js because js allows nicer comments
 module.exports = {
+  // ALL browsers are defined in /tests/browsers/
   browsers: {
     // if this is filled - only those tests will be tested
     // TODO: add extension automatically
     //only: ["win8.1_ie"],//, "safari_9.1", "chrome", "edge", "firefox"],
     //only: ["win8.1_ie", "safari_9.1", "chrome", "edge", "firefox"],
-    only: ['local'],
+    only: ['local.phantom'],
     // add here some tests to skip
     skip: []
   },

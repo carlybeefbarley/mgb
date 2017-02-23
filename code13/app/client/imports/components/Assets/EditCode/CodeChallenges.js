4b7f7eb33f7020c008f59be7778d44a78cde4e39
David Golds <david.p.golds@gmail.com>
2017-02-23 13:02:38 -0800

    refactor(CodeChallenges): update to current coding conventions

--- code13/app/client/imports/components/Assets/EditCode/CodeChallenges.js
+++ code13/app/client/imports/components/Assets/EditCode/CodeChallenges.js
@@ -1,14 +1,7 @@
 import _ from 'lodash'
 import React, { PropTypes } from 'react'
 import ReactDOM from 'react-dom'
-import {
-  Button,
-  Divider,
-  Header,
-  Icon,
-  Image,
-  Label,
-} from 'semantic-ui-react'
+import { Button, Modal, Icon, List } from 'semantic-ui-react'
 
 import { makeCDNLink } from '/client/imports/helpers/assetFetchers'
 import SkillNodes from '/imports/Skills/SkillNodes/SkillNodes'
@@ -20,72 +13,66 @@ import './editcode.css'
 
 export default class CodeChallenges extends React.Component {
 
+  static propTypes = {
+    currUser:    PropTypes.object,
+    skillPath:   PropTypes.string,
+    codeMirror:  PropTypes.object,
+    active:      PropTypes.bool
+  }
+
   constructor(props) {
     super(props)
-
-    // console.log(this.props.skillPath)
-    
     this.skillNode = this.getSkillNode(props.skillPath)
     const skillArr = props.skillPath.split('.')
     this.skillName = skillArr[skillArr.length-1]
-
     this.state = {
-      results: []
+      results:                    [],       // Array of results we get back from the iFrame that runs the tests
+      showAllTestsCompletedModal: false     // true if we want to show the All Tests Completed Modal
     }
-
   }
 
   componentDidMount() {
-    // console.log('component did mount')
     this.getReference()
-
     window.addEventListener("message", this.receiveMessage.bind(this), false)
-
   }
 
   getReference() {
     this.iFrame = ReactDOM.findDOMNode(this.refs.iFrameTests)
-    this.testsSuccessModal = ReactDOM.findDOMNode(this.refs.testsSuccessModal)
   }
 
-  getSkillNode(){
+  getSkillNode() {
     let skillNodes = _.cloneDeep(SkillNodes)
     const path = this.props.skillPath.split('.')
-    while(path.length > 0){
+    while (path.length > 0)
       skillNodes = skillNodes[path.shift()]
-    }
     return skillNodes
   }
 
-  receiveMessage(e){
-    if(e.data.prefix && e.data.prefix == 'codeTests'){
-      // console.log(e.data.results)
+  receiveMessage(e) {
+    if (e.data.prefix && e.data.prefix == 'codeTests') {
       this.setState({ results: e.data.results })
       
       let totalSuccess = true
       e.data.results.map((result) => {
-        if(!result.success){
+        if (!result.success)
           totalSuccess = false
-        }
       })
-      if(totalSuccess)
+      if (totalSuccess)
         this.successPopup()
     }
   }
 
-  successPopup(){
+  successPopup() {
     const skillPath = this.props.skillPath + '.' + this.skillName
     // console.log('skill learned', skillPath)
-
     // TODO show notification for user
     learnSkill( skillPath )
 
-    $(this.testsSuccessModal).modal('show')
+    this.setState( { showAllTestsCompletedModal: true } )
   }
 
-  runTests(){
+  runTests() {
     const tests = this.skillNode.$meta.tests
-
     const message = {
       code: this.props.codeMirror.getValue(),
       tests: tests
@@ -94,7 +81,7 @@ export default class CodeChallenges extends React.Component {
     this.iFrame.contentWindow.postMessage(message, "*")
   }
 
-  resetCode(){
+  resetCode() {
     const newCode = this.skillNode.$meta.code.join( '\n' )
     this.props.codeMirror.setValue(newCode)
   }
@@ -107,31 +94,30 @@ export default class CodeChallenges extends React.Component {
     const jsSkills = SkillNodes.code.js.basics
     const skillsArr = []
     for (let key in jsSkills) {
-      if (jsSkills.hasOwnProperty( key ) && key != '$meta') {
+      if (jsSkills.hasOwnProperty( key ) && key != '$meta')
         skillsArr.push(key)
-      }
     }
 
     const idx = skillsArr.indexOf(this.skillName)
 
-    if(idx < skillsArr.length-1){
+    if (idx < skillsArr.length-1) {
       const nextSkillName = skillsArr[idx+1]
       const code = SkillNodes.code.js.basics[nextSkillName].$meta.code.join( '\n' )
       // console.log(nextSkillName, code)
 
-      $(this.testsSuccessModal).modal('hide')
+      this.setState( { showAllTestsCompletedModal: false } )
       StartCodeJsRoute(nextSkillName, code, this.props.currUser)
-    } else {
-      // TODO replace with proper modal
-      alert('Congratulations! You have finished JavaScript basics challenges.')
+    } 
+    else
+    {
+      this.setState( { showAllTestsCompletedModal: false } )
+      alert('Congratulations! You have finished the JavaScript basics challenges!')
     }
-
-
-    
   }
 
   render() {
     const description = this.skillNode.$meta.description
+    const { showAllTestsCompletedModal } = this.state
 
     return (
       <div className={"content " +(this.props.active ? "active" : "")}>
@@ -145,21 +131,27 @@ export default class CodeChallenges extends React.Component {
           <Icon name='help' /> Help
         </Button>
 
+        <List relaxed>
         {
-          this.state.results.map((result, idx) => (
-            <div key={idx}>
-              <i className={"icon circle " + (result.success ? "check big green" : "minus big red")}></i>
-              {result.message}
-            </div>
+          this.state.results.map(result => (
+            <List.Item key={result}>
+              <List.Icon 
+                  size='big'
+                  name={`circle ${result.success ? 'check' : 'minus'}`}
+                  color={result.success ? 'green' : 'red'} />
+              <List.Content>
+                <span dangerouslySetInnerHTML={{ __html: result.message}} />
+              </List.Content>
+            </List.Item>
           ))
         }
+        </List>
 
         {
           description.map((text, i) => (
-            <div key={i}>{text}</div>
+            <div key={i} dangerouslySetInnerHTML={{ __html: text}} />
           ))
         }
-
 
         <iframe
           style={{ display: "none", width: "10px", height: "10px" }}
@@ -171,18 +163,28 @@ export default class CodeChallenges extends React.Component {
           >
         </iframe>
 
-        <div className="ui small modal" ref="testsSuccessModal">
-          <div className="content">
-            <h2 style={{textAlign:"center"}}><i className="check big green circle icon"></i>Success</h2>
-            <br/><br/>
-            <button style={{display:"block", margin:"0 auto"}} onClick={this.nextChallenge.bind(this)} className="positive ui button">
-              Next challenge
-            </button>
-          </div>
-        </div>
-
+        { showAllTestsCompletedModal && (
+            <Modal 
+                closeOnDocumentClick={false} 
+                closeOnRootNodeClick={false}
+                defaultOpen >
+              <Modal.Header>
+                <Icon size='big' color='green' name='check circle' />
+                Success
+              </Modal.Header>
+              <Modal.Content>
+                You completed this Code Challenge
+              </Modal.Content>
+              <Modal.Actions>
+                <Button 
+                    positive
+                    content='Next challenge'
+                    onClick={this.nextChallenge.bind(this)} />
+              </Modal.Actions>
+            </Modal>
+          )
+        }
       </div>
     )
   }
-
 }

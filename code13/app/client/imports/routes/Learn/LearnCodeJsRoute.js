16c93f4d34a4433ac3ddf55483562bde25925bce
David Golds <david.p.golds@gmail.com>
2017-02-23 17:56:51 -0800

    refactor(LearnCodeJsRoute): simplify reindex of skillnode and use less memory; simplify other code

--- code13/app/client/imports/routes/Learn/LearnCodeJsRoute.js
+++ code13/app/client/imports/routes/Learn/LearnCodeJsRoute.js
@@ -9,16 +9,10 @@ import { logActivity } from '/imports/schemas/activity'
 
 import SkillNodes from '/imports/Skills/SkillNodes/SkillNodes'
 
-const jsSkills = SkillNodes.code.js.basics
-const skillItems = []
-for (let key in jsSkills) {
-  if (jsSkills.hasOwnProperty( key ) && key != '$meta') {
-    let skill = _.cloneDeep( jsSkills[key]['$meta'] )
-    skill.idx = key
-    skill.skill = jsSkills[key].skill
-    skillItems.push( skill )
-  }
-}
+// for convenience, extract out the skillItems' $meta content into a simple array
+const skillItems = _.compact(_.map(SkillNodes.code.js.basics, (val, key) => (
+  key === '$meta' ? null : { ...val['$meta'], idx: key, skill: val.skill, key: key }
+)))
 
 const handleClick = (e, idx, code, currUser) => {
   const newTab = (e.buttons == 4 || e.button == 1)
@@ -32,7 +26,7 @@ export const StartCodeJsRoute = (name, code, currUser, newTab) => {
     return
   }
 
-  let newAsset = {
+  const newAsset = {
     name: 'tutorials.js.' + name,
     kind: 'code',
     skillPath: 'code.js.basics.' + name,
@@ -45,52 +39,46 @@ export const StartCodeJsRoute = (name, code, currUser, newTab) => {
   Meteor.call( 'Azzets.create', newAsset, (error, result) => {
     if (error) {
       showToast( "cannot create Asset because: " + error.reason, 'error' )
-    } else {
-      newAsset._id = result             // So activity log will work
-      logActivity( "asset.create", `Created code tutorial`, null, newAsset )
-
-      const url = `/u/${currUser.username}/asset/${result}`
-
-      if (newTab) {
-        window.open( window.location.origin + url )
-      } else {
-        utilPushTo( window.location, url )
-      }
+      return
     }
-  } )
+    newAsset._id = result             // So activity log will work
+    logActivity( "asset.create", 'Created code tutorial', null, newAsset )
+    const url = `/u/${currUser.username}/asset/${newAsset._id}`
+
+    if (newTab)
+      window.open( window.location.origin + url )
+    else
+      utilPushTo( window.location, url )
+  })
 }
 
-const LearnCodeJsRoute = (props, context) => {
-  const currUser = props.currUser
-
-  return (
-    <Grid container columns='1'>
-      <Divider hidden />
-      <Grid.Column>
-        <Header
-          as='h1'
-          content='JavaScript programming basics'
-          subheader='Click on item and explore it'
-        />
-      </Grid.Column>
-      <Grid.Column>
-        <Segment padded piled>
-          <List size='large' relaxed='very' link className="skills">
-            { skillItems.map( (area, idx) => (
-              <List.Item
-                as='a'
-                key={idx}
-                header={area.name}
-                icon={area.icon}
-                onClick={ (e) => handleClick( e, area.idx, area.code, currUser ) }
-              />
-            ) ) }
-          </List>
-        </Segment>
-      </Grid.Column>
-      (Note: Just two here for current testing. Many more coming soon...)
-    </Grid>
-  )
-}
+const LearnCodeJsRoute = ( { currUser } ) => (
+  <Grid container columns='1'>
+    <Divider hidden />
+    <Grid.Column>
+      <Header
+        as='h1'
+        content='JavaScript programming basics'
+        subheader='Click on an item and explore it'
+      />
+    </Grid.Column>
+    <Grid.Column>
+      <Segment padded piled>
+        <List size='large' relaxed='very' link className="skills">
+          { skillItems.map( (area, idx) => (
+            <List.Item
+              as='a'
+              key={idx}
+              header={area.name}
+              icon={area.icon}
+              onClick={ (e) => handleClick( e, area.idx, area.code, currUser ) }
+            />
+          ) ) }
+        </List>
+      </Segment>
+    </Grid.Column>
+    (Note: Just two here for current testing. Many more coming soon...)
+  </Grid>
+)
 
 export default LearnCodeJsRoute

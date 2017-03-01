6e7c8f76e5265237eaed83b98eb5d1344fb6bf70
Guntis <guntis.smaukstelis@gmail.com>
2017-03-01 18:22:22 +0200

    opening tutorial from jsGames route

--- code13/app/client/imports/routes/Learn/LearnJsGamesRoute.js
+++ code13/app/client/imports/routes/Learn/LearnJsGamesRoute.js
@@ -1,8 +1,11 @@
 import _ from 'lodash'
 import React, { PropTypes } from 'react'
 import styles from '../home.css'
-import QLink from '../QLink'
+import { utilPushTo } from "/client/imports/routes/QLink"
 import { Divider, Grid, Header, List, Segment } from 'semantic-ui-react'
+
+import { showToast } from '/client/imports/routes/App'
+import { logActivity } from '/imports/schemas/activity'
 
 import SkillNodes from '/imports/Skills/SkillNodes/SkillNodes'
 import SkillsMap from '/client/imports/components/Skills/SkillsMap.js'
@@ -53,8 +56,42 @@ for (var key in jsGamesSkills) {
 //   },
 // ]
 
-const handleClick = (e, skillPath) => {
+const handleClick = (e, idx, code, currUser) => {
+  const newTab = (e.buttons == 4 || e.button == 1)
+  StartJsGamesRoute(idx, code, currUser, newTab)
+}
 
+export const StartJsGamesRoute = (name, code, currUser, newTab) => {
+  if (!currUser)
+  {
+    showToast( 'You must be logged in to use these tutorials', 'info')
+    return
+  }
+
+  const newAsset = {
+    name: 'tutorials.jsGame.' + name,
+    kind: 'code',
+    skillPath: 'code.js.games.' + name,
+    content2: { src: code },
+    isCompleted: false,
+    isDeleted: false,
+    isPrivate: false
+  }
+
+  Meteor.call( 'Azzets.create', newAsset, (error, result) => {
+    if (error) {
+      showToast( "cannot create Asset because: " + error.reason, 'error' )
+      return
+    }
+    newAsset._id = result             // So activity log will work
+    logActivity( "asset.create", 'Created game tutorial', null, newAsset )
+    const url = `/u/${currUser.username}/asset/${newAsset._id}`
+
+    if (newTab)
+      window.open( window.location.origin + url )
+    else
+      utilPushTo( window.location, url )
+  })
 }
 
 const LearnJsGamesRoute = ({ currUser }, context) => {
@@ -70,10 +107,10 @@ const LearnJsGamesRoute = ({ currUser }, context) => {
           </Header.Subheader>
         </Header>
         { currUser && (
-        <div style={{ clear: 'both' }}>
-          <SkillsMap user={currUser} subSkill={true} onlySkillArea={'code.js.games'} userSkills={context.skills} ownsProfile={true} />
-        </div>
-      )}
+          <div style={{ clear: 'both' }}>
+            <SkillsMap user={currUser} subSkill={true} onlySkillArea={'code.js.games'} userSkills={context.skills} ownsProfile={true} />
+          </div>
+        )}
       </Grid.Column>
       <Grid.Column>
         <Segment padded piled>
@@ -87,7 +124,7 @@ const LearnJsGamesRoute = ({ currUser }, context) => {
                   key={idx}
                   as={'a'} // isComplete ? 'div' : 'a'}
                   //disabled={isComplete}
-                  onClick={ (e) => handleClick( e, skillPath ) }
+                  onClick={ (e) => handleClick( e, area.idx, '// hello world games!', currUser ) }
                   icon={isComplete ? { name: 'checkmark', color: 'green' } : area.icon}
                   header={isComplete ? null : area.name}
                   content={isComplete ? area.name : area.description}

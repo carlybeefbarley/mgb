cbdba6662e64698006bf0e25f112d5cfbcd92e30
Guntis <guntis.smaukstelis@gmail.com>
2017-03-01 17:15:14 +0200

    learn mole route gets data from skillNodes

--- code13/app/client/imports/routes/Learn/LearnCodeMoleRoute.js
+++ code13/app/client/imports/routes/Learn/LearnCodeMoleRoute.js
@@ -4,38 +4,58 @@ import styles from '../home.css'
 import QLink from '../QLink'
 import { Divider, Grid, Header, List, Segment } from 'semantic-ui-react'
 
-const jsItems = [
-  {
-    icon: 'code',
-    link: '/u/!vault/project/2suHPANwpaN5Pjumc',
-    content: 'Basic gameplay',
-    desc: ``
-  },
-  {
-    icon: 'code',
-    link: '/u/!vault/project/aCdy9zz5cJjNog2en',
-    content: 'Tweens',
-    desc: ``
-  },
-  {
-    icon: 'code',
-    link: '/u/!vault/project/NwobuqkQqrcuzzAeo',
-    content: 'Timing',
-    desc: ``
-  },
-  {
-    icon: 'code',
-    link: '/u/!vault/project/PHjAGkS9L4mTTPepE',
-    content: 'User interface',
-    desc: ``
-  },
-  {
-    icon: 'code',
-    link: '/u/!vault/project/JqN5CbdnNFZZqBXnE',
-    content: 'OOP',
-    desc: `Refactor existing game OOP style.`
-  },
-]
+import SkillNodes from '/imports/Skills/SkillNodes/SkillNodes'
+import SkillsMap from '/client/imports/components/Skills/SkillsMap.js'
+
+
+const jsGamesSkills = SkillNodes.code.js.games
+const skillItems = []
+for (var key in jsGamesSkills) {
+  if (jsGamesSkills.hasOwnProperty( key ) && key != '$meta') {
+    let skill = _.cloneDeep( jsGamesSkills[key]['$meta'] )
+    skill.idx = key
+    skillItems.push( skill )
+  }
+}
+
+// console.log(_jsGamesSkillsNode)
+
+// const jsItems = [
+//   {
+//     icon: 'code',
+//     link: '/u/!vault/project/2suHPANwpaN5Pjumc',
+//     content: 'Basic gameplay',
+//     desc: ``
+//   },
+//   {
+//     icon: 'code',
+//     link: '/u/!vault/project/aCdy9zz5cJjNog2en',
+//     content: 'Tweens',
+//     desc: ``
+//   },
+//   {
+//     icon: 'code',
+//     link: '/u/!vault/project/NwobuqkQqrcuzzAeo',
+//     content: 'Timing',
+//     desc: ``
+//   },
+//   {
+//     icon: 'code',
+//     link: '/u/!vault/project/PHjAGkS9L4mTTPepE',
+//     content: 'User interface',
+//     desc: ``
+//   },
+//   {
+//     icon: 'code',
+//     link: '/u/!vault/project/JqN5CbdnNFZZqBXnE',
+//     content: 'OOP',
+//     desc: `Refactor existing game OOP style.`
+//   },
+// ]
+
+const handleClick = (e, skillPath) => {
+
+}
 
 const LearnCodeMoleRoute = ({ currUser }, context) => {
   return (
@@ -53,21 +73,31 @@ const LearnCodeMoleRoute = ({ currUser }, context) => {
       <Grid.Column>
         <Segment padded piled>
           <List size='large' relaxed='very' link className="skills">
-            { jsItems.map( (area, idx) => (
-              <List.Item
-                key={idx}
-                as={QLink}
-                to={area.link}
-                header={area.content}
-                content={area.desc}
-                icon={area.icon}
-              />
-            ) ) }
+            { skillItems.map( (area, idx) => {
+              let skillPath = 'code/js/games/' + area.idx + '/' + area.idx
+              const isComplete = currUser && !_.isEmpty( context.skills[skillPath] )
+
+              return (
+                <List.Item
+                  key={idx}
+                  as={'a'} // isComplete ? 'div' : 'a'}
+                  //disabled={isComplete}
+                  onClick={ (e) => handleClick( e, skillPath ) }
+                  icon={isComplete ? { name: 'checkmark', color: 'green' } : area.icon}
+                  header={isComplete ? null : area.name}
+                  content={isComplete ? area.name : area.description}
+                />
+              )
+            } ) }
           </List>
         </Segment>
       </Grid.Column>
     </Grid>
   )
+}
+
+LearnCodeMoleRoute.contextTypes = {
+  skills: PropTypes.object       // skills for currently loggedIn user (not necessarily the props.user user)
 }
 
 export default LearnCodeMoleRoute

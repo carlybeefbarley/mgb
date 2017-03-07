a24e1efdf22b5e589eb57ba67f37baf3c4c12b86
Guntis <guntis.smaukstelis@gmail.com>
2017-03-07 17:11:45 +0200

    if code challenge asset exists, dont create another one

--- code13/app/client/imports/routes/Learn/LearnCodeJsRoute.js
+++ code13/app/client/imports/routes/Learn/LearnCodeJsRoute.js
@@ -10,6 +10,9 @@ import { logActivity } from '/imports/schemas/activity'
 import SkillNodes from '/imports/Skills/SkillNodes/SkillNodes'
 import SkillsMap from '/client/imports/components/Skills/SkillsMap.js'
 
+import {getAssetBySelector} from '/client/imports/helpers/assetFetchers'
+
+
 // for convenience, extract out the skillItems' $meta content into a simple array
 const skillItems = _.compact(_.map(SkillNodes.code.js.basics, (val, key) => (
   key === '$meta' ? null : { ...val['$meta'], idx: key, skill: val.skill, key: key }
@@ -33,27 +36,43 @@ export const StartCodeJsRoute = (name, code, currUser, newTab) => {
   const newAsset = {
     name: 'tutorials.js.' + name,
     kind: 'code',
-    skillPath: 'code.js.basics.' + name,
-    content2: { src: code },
-    isCompleted: false,
     isDeleted: false,
-    isPrivate: false
+    dn_ownerName: currUser.username
   }
 
-  Meteor.call( 'Azzets.create', newAsset, (error, result) => {
-    if (error) {
-      showToast( "cannot create Asset because: " + error.reason, 'error' )
-      return
+  // check if asset exists
+  getAssetBySelector(newAsset, (asset, err) => {
+    if (asset)  // asset exists. open it.
+    {  
+      const url = `/u/${asset.dn_ownerName}/asset/${asset._id}`
+      openUrl(url, newTab)
     }
-    newAsset._id = result             // So activity log will work
-    logActivity( "asset.create", 'Created code tutorial', null, newAsset )
-    const url = `/u/${currUser.username}/asset/${newAsset._id}`
-
-    if (newTab)
-      window.open( window.location.origin + url )
-    else
-      utilPushTo( window.location, url )
+    else        // asset doesn't exist. create one.
+    {  
+      newAsset.skillPath = 'code.js.basics.' + name
+      newAsset.content2 = { src: code }
+      newAsset.isCompleted = false
+      newAsset.isPrivate = false
+    
+      Meteor.call( 'Azzets.create', newAsset, (error, result) => {
+        if (error) {
+          showToast( "cannot create Asset because: " + error.reason, 'error' )
+          return
+        }
+        newAsset._id = result             // So activity log will work
+        logActivity( "asset.create", 'Created code tutorial', null, newAsset )
+        const url = `/u/${currUser.username}/asset/${newAsset._id}`
+        openUrl(url, newTab)
+      })
+    }
   })
+}
+
+const openUrl = (url, newTab) => {
+  if (newTab)
+    window.open( window.location.origin + url )
+  else
+    utilPushTo( window.location, url )
 }
 
 const LearnCodeJsRoute = ( { currUser }, context ) => (

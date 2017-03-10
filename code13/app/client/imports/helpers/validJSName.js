a215712d0e51a03e9f7eb17afc09a77b30ff9bd7
David Golds <david.p.golds@gmail.com>
2017-03-10 14:56:18 -0800

    bugfix(makeVaildJsName): clarify and handle more cases

--- code13/app/client/imports/helpers/validJSName.js
+++ code13/app/client/imports/helpers/validJSName.js
@@ -1,27 +1,20 @@
-const isLetter = /[a-zA-Z]/
-const isNum = /^\d*/
-const isNotAlphanumeric = /([^a-zA-Z\d])/gi // !!!notice NOT(^)
-// converts asset name to valid JS name
-// e.g. 123My.cool.lib7 => MyCoolLib7
-export default (str) => {
-  // strip first number or other fancy char - and replace non alphanumeric
-  // this can be improved to allow some UTF-8 symbols as variables
-  // pick some fancy charcode \x0001 instead of $ ?
-  const stripped = str.replace(isNum, '').replace(isNotAlphanumeric, '$')
-  let ret = ''
-  for (let i = 0; i < stripped.length; i++) {
-    if (stripped[i] == '$') {
-      i++
-      if (stripped[i] == '$')
-        continue
+import _ from 'lodash'
 
-      if (stripped[i] && isLetter.test(stripped[i]))
-        ret += stripped[i].toUpperCase()
-      else
-        ret += stripped[i] // number
-    }
-    else
-      ret += stripped[i]
-  }
-  return ret
-}
+/**
+ * 
+ * @param {String} this is expectied to be an assetName. See validate.js for expected values
+ * @returns {String} a valid JavaScript variable name derived from the input, or 'importedVar' as a last resort
+ */
+const makeValidJsVarName = str => 
+  _.chain(str)
+  .deburr()                     // simplify diacriticals
+  .trimStart('0123456789.- ()!/~;\'<>@&') // no leading digits or weird punctuation in variables
+  .words(/[a-zA-Z0-9_]+/g)      // extract regions (alphanumeric 'words' ) that can be used in variableNames
+  .camelCase()                  // camelCase it (an alternative is join('_') for snakeCase)
+  .value() || 'importedVar'     // return it, or 'importedVar' if there was nothing left
+
+// For sanity check when making changes...
+// const _tests = [ '123My.cool.lib7', 'abc123', '123abc', '!/s.aBc(12)3', '(!abc1(23', ';11abc123', '\'_foo_M<__>x_@_y@z __&']
+// _.each(_tests, t => console.log(`'${t}' -> '${makeValidJsVarName(t)}'`))
+
+export default makeValidJsVarName

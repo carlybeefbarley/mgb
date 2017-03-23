0dc34cea761cbf04327b9c94a2b0f234aeaedb7e
David Golds <david.p.golds@gmail.com>
2017-03-23 13:15:17 -0700

    Add script to deploy to staging.mygamebuilder.com

--- code13/app/deploy.sh
+++ code13/app/deploy.sh
@@ -1,2 +1,1 @@
-#METEOR_BINARY_DEP_WORKAROUND=t 
 DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy v2.mygamebuilder.com --settings settings.json
---
+++ code13/app/stage.sh
@@ -0,0 +1,1 @@
+DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy staging.mygamebuilder.com --settings settings-staging.json

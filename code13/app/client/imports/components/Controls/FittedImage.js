d923142992b03ed4e83371f2b57a10a2b7909c6c
Leah Elisabeth Sheryl Renee <leah@mycodebuilder.com>
2017-03-31 15:19:46 -0700

    improved fittedImage to avoid loss of contain tag

--- code13/app/client/imports/components/Controls/FittedImage.js
+++ code13/app/client/imports/components/Controls/FittedImage.js
@@ -1,16 +1,16 @@
 import React, { PropTypes } from "react";
 
-export default FittedImage = (
-  { src, height = "140px", ...rest }
-) => // This is <div> instead of <img> so that it won't have the border that chrome puts on if src has no content
-(
+export default (FittedImage = (
+  { src, height = "140px", width, ...rest } // This is <div> instead of <img> so that it won't have the border that chrome puts on if src has no content
+) => (
   <div
     className="mgb-pixelated"
+    crossOrigin="anonymous"
     style={{
-      background: `url(${src}) no-repeat center`,
+      background: `url(${src}) no-repeat center / contain`,
       height: height,
-      backgroundSize: "contain"
+      width: width
     }}
     {...rest}
   />
-);
+));

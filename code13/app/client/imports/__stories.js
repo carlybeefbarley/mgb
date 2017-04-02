54743b2dbcead64204ac4cefa68468b3de1a0f92
David Golds <david.p.golds@gmail.com>
2017-04-02 11:35:55 -0700

    fixed base storybook example

--- code13/app/client/imports/__stories.js
+++ code13/app/client/imports/__stories.js
@@ -27,8 +27,42 @@ fakeAppRouteProps.handleSetCurrentlyEditingAssetInfo = action('Invoked handleSet
 // function using the format of ' import from "meteor/foo" '. 
 // Hey, that's a Good Reason to keep stuff well abstracted :)
 
-// Here's a test of the simple
-import DashboardRoute from './routes/Dashboard/DashboardRoute'
+// Here's a test of the simple dashboard...
+  // import DashboardRoute from './routes/Dashboard/DashboardRoute'
 
-storiesOf('DashBoard', module)
-  .add('Dashboard', () => <DashboardRoute {...fakeAppRouteProps}/>)
+  // storiesOf('DashBoard', module)
+  //   .add('Dashboard', () => <DashboardRoute {...fakeAppRouteProps}/>)
+
+
+
+import { Card, Image, Icon } from 'semantic-ui-react'
+
+
+const CardExampleCard = () => (
+  <Card>
+    <Image src='/images/mascots/bigguy.png' />
+    <Card.Content>
+      <Card.Header>
+        Matthew
+      </Card.Header>
+      <Card.Meta>
+        <span className='date'>
+          Joined in 2015
+        </span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+)
+
+storiesOf('Examples', module)
+  .add('Card', () => <CardExampleCard />)
+

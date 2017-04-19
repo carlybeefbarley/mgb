38cf4be1aa3047a8edc5d8871c6a6e1d84431e3a
Leah Elisabeth Sheryl Renee <leah@mycodebuilder.com>
2017-04-19 11:08:31 -1000

    fix #641 replaced fake home meet users column with meteor pub/sub query to get real user data for home meet friends column

--- code13/app/client/imports/components/Home/HomeMeetFriendsColumn.js
+++ code13/app/client/imports/components/Home/HomeMeetFriendsColumn.js
@@ -4,28 +4,22 @@ import getStartedStyle from '/client/imports/routes/GetStarted.css'
 
 import { Grid, Header, List, Icon, Image, Button } from 'semantic-ui-react'
 import QLink from '/client/imports/routes/QLink'
+import { createContainer } from 'meteor/react-meteor-data'
+import { Users } from '/imports/schemas'
+import { userSorters } from '/imports/schemas/users'
 
-const _propTypes = {
-}
-
-const fakePeopleList = [ 
-  { img: '/api/asset/png/kvBq9PB987zuKiENQ', name: 'dgolds', badges: 9 },
-  { img: 'http://www.gravatar.com/avatar/747ac1809af200d4f8403cfe6a240c9e?s=50&d=mm', name: 'DaPerson', badges: 3 },
-  { img: 'http://semantic-ui.com/images/avatar/small/christian.jpg', name: 'Micah', badges: 6 }
-]
-
-const HomeMeetFriendsColumn = () => (
+const HomeMeetFriendsColumnUI = (props) => (
   <Grid.Column className='animated fadeIn'>
     <Header as='h2' style={{ marginBottom: '1em' }}>Meet creative friends</Header>
     <List className="very relaxed">
       {
-        fakePeopleList.map( (person, idx) => (
-          <List.Item key={idx}>
-    
-            <Image avatar verticalAlign='middle' style={{ height: 60, width: 60 }} src={person.img} />
+        props.loading === false && 
+        props.userList.map( (person, idx) => (
+          <List.Item key={idx} as={QLink} to={`/u/${person.username}`}>
+            <Image avatar verticalAlign='middle' style={{ height: 60, width: 60 }} src={person.profile.avatar} />
             <div className="content middle aligned" style={{ marginLeft: '1em' }}>
-              <Header as='h3' content={person.name} />
-              <p><Icon name='trophy' />{person.badges} badges</p>
+              <Header as='h3' content={person.username} />
+              <p><Icon name='trophy' />{person.badges ? person.badges.length : 0} badges</p>
             </div>
           </List.Item>
         ))
@@ -37,6 +31,21 @@ const HomeMeetFriendsColumn = () => (
     </QLink>
   </Grid.Column>
 )
-
-HomeMeetFriendsColumn.propTypes = _propTypes
+const HomeMeetFriendsColumn = createContainer ( 
+  () => {
+    const usersHandle = Meteor.subscribe("users.frontPageList")
+    let findOpts = {
+      sort:   userSorters["createdNewest"],
+      limit:  3
+    }
+    let selector = {
+      badges: {$exists: true},
+      "badges.0":{$exists: true}
+    }
+    return {
+      userList: Users.find(selector, findOpts).fetch(),
+      loading: !usersHandle.ready()
+    }
+  },
+  HomeMeetFriendsColumnUI)
 export default HomeMeetFriendsColumn

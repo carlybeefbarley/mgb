f18dc4857eab1c15ea358deccc24f55fa985a520
David Golds <david.p.golds@gmail.com>
2017-04-25 23:35:21 -0700

    landing page: text tweaks

--- code13/landing/src/js/main.js
+++ code13/landing/src/js/main.js
@@ -31,11 +31,10 @@ var setMenuTransparency = throttle( function (e) {
   $menu.style.background = 'rgba(20, 150, 160, ' + inRatio + ')'
   $menu.style.boxShadow = '0 ' + inRatio * 5 + 'px ' + inRatio * 10 + 'px rgba(0, 0, 0, ' + inRatio * 0.25 + ')'
   $heroCaption.style.opacity=(heroCaptionRect.bottom / heroCaptionRect.height)
-  if (inRatio >= 1) {
+  if (inRatio >= 1)
     $menuSignupButton.classList.remove('basic')
-  } else {
+  else
     $menuSignupButton.classList.add('basic')
-  }
 } )
 
 // set initial transparency
@@ -47,7 +46,7 @@ window.addEventListener( 'resize', setMenuTransparency )
 
 
 console.log(
-'Hello Curious hacker-type people who like to look under the hood!\n' + 
+'%cHello Curious hacker-type people who like to look under the hood!\n' + 
 'How r u?\n\n' + 
 'Since we are here, and you seem to know your way around a bit, here\'s the deal...\n'+
 '1. This site is primarily designed as a great + _frictionless_ environment for people to learn to code _substantial_ projects.. Learning by Doing!\n'+
@@ -65,7 +64,8 @@ console.log(
 'Q: Do we intend to support git?\nA: Yes, but not initially... we are initially pushing a google-docs + semver-packages model for small groups, and will add git workflows later. We also believe that kids will appreciate truly version control mostly once they have mangled some files first, so it is a semi-intentional part of the learning experience...\n'+
 '\nQ: Why are projects so weird?\nA: Because we want to create a frictionless free-flowing system as a priority, so for now projects are tags.\n   This makes it easy to use parts of one game in another.\n   We will support strict projects (an asset can only be in one project) in future.\n'+
 '\nQ: Do you have 2048 example?\nA: yes! Hi HN folks!  We also have a whack-a-mole example of course\n'+
-'\nQ: Why just JavaScript?\nA: Focus, and simplicity. There are lots of other great languages, but JavaScript (ES6) can be presented simply for learning, but now has pretty fancy features including async capabilities. JS is nearly always going to be encountered in browser scenarios, and is good-enough on the server for the most common needs. Sorry Rust/Haskell fans, we love your cool stuff though...\n'+
+'\nQ: Why just JavaScript?\nA: Focus, and simplicity. There are lots of other great languages, but JavaScript (ES6) can be presented simply for learning, and now has pretty fancy features including async capabilities. JS is nearly always going to be encountered in browser scenarios, and is good-enough on the server for the most common needs. Sorry Rust/Haskell fans, we love your cool stuff though...\n'+
 '\nQ: Oooh. server?\nA: Oh yes!!! Server multiplayer stuff is on the horizon. Our design goal is to enable users to easily implement .io style games for example. Should be fun!\n'+
-'\nQ: I want to know more!\nA: Come chat with us in-system, or tweet to @dgolds\n ^^^ GLHF ^^^\n'
+'\nQ: I want to know more!\nA: Come chat with us in-system, or tweet to @dgolds\n ^^^ GLHF ^^^\n',
+'color: #5c2197' // http://bada55.io ftw
 )

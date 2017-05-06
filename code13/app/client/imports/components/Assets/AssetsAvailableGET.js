42bb043980a49972b37426ffc77d48aed779e871
David Golds <david.p.golds@gmail.com>
2017-05-05 18:20:45 -0700

    project with no assets - don't show a random zero

--- code13/app/client/imports/components/Assets/AssetsAvailableGET.js
+++ code13/app/client/imports/components/Assets/AssetsAvailableGET.js
@@ -20,7 +20,7 @@ const AssetItems = ( { assets } ) => (
     {(!assets || assets.length === 0) &&
       <Segment basic>No assets yet</Segment>}
     {_.map(assets, a => <AssetCard classNames='mgb-assetcard-width' allowDrag={true} asset={a} key={a._id} />) }
-    { assets && assets.length && <div data-tooltip={`Only the ${_showLimit} most recently edited assets are shown here`} style={{ minWidth: '8em', textAlign: 'center', margin: 'auto' }}>. . .</div>}
+    { assets && assets.length > 0 && <div data-tooltip={`Only the ${_showLimit} most recently edited assets are shown here`} style={{ minWidth: '8em', textAlign: 'center', margin: 'auto' }}>. . .</div>}
   </Card.Group>
 );
 

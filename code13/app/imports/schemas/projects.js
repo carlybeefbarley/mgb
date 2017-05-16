6171b76d71335928b168410e6582b09f68a11859
David Golds <david.p.golds@gmail.com>
2017-05-15 18:44:09 -0700

    bugfix: re-enable project update for projects with no members!

--- code13/app/imports/schemas/projects.js
+++ code13/app/imports/schemas/projects.js
@@ -488,7 +488,7 @@ Meteor.methods({
       throw new Meteor.Error(404, 'Project Id does not exist')
     if (existingProjectRecord.ownerId !== this.userId)
       throw new Meteor.Error(401, "You don't have permission to edit this")
-    if(data.memberIds.length > _calcMaxNumMembersAllowedInProject(Meteor.user()) )
+    if(data.memberIds && data.memberIds.length > _calcMaxNumMembersAllowedInProject(Meteor.user()) )
       throw new Meteor.Error(401, "You have exceeded maximum number of members allowed")
 
     // 1. Create new Project record and store in Collection

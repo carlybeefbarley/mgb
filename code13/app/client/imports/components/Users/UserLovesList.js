27bdd093fc194fde850699b2dff7353524c9e00d
David Golds <david.p.golds@gmail.com>
2017-05-23 16:09:06 -0700

    UserLovesList: Add qlink to asset

--- code13/app/client/imports/components/Users/UserLovesList.js
+++ code13/app/client/imports/components/Users/UserLovesList.js
@@ -1,42 +1,48 @@
+import _ from 'lodash'
 import React, { PropTypes } from 'react'
-import { Grid, List, Header } from "semantic-ui-react";
+import { Header } from "semantic-ui-react"
 import { createContainer } from 'meteor/react-meteor-data'
 import { Activity } from '/imports/schemas'
 import moment from 'moment'
-import _ from 'lodash'
 
-const UserLovesListUI = props => (
+const UserLovesListUI = ( { user, loveAct } ) => (
   <div>
-  <Header as="h2" content="Your asset love history"/>
-  {_.map(props.loveAct, a => {
-    const ago = moment(a.timestamp).fromNow()
-    return (
-    <div key={a._id} >
-    <span>You loved {a.toAssetName} by </span>
+    <Header as="h2" content={`${user ? user.username : '???'}'s asset love history`}/>
+    { _.map(loveAct, a => (
+      <div key={a._id} >
+        <span>You loved </span>
+        <QLink to={`/u/${a.byOwnerName}/asset/${a.toAssetId}`}>
+          {a.toAssetName}
+        </QLink>
+        <span> by </span>
         <QLink to={`/u/${a.byOwnerName}`}>
           {a.toOwnerName}
         </QLink>
-        <small style={{color: '#c8c8c8'}}>&ensp;{ago}</small>
-    </div>
-    )
-  })}
+        <small style={{color: '#c8c8c8'}}>&ensp;{moment(a.timestamp).fromNow()}</small>
+      </div>
+      )
+    ) }
   </div>
 )
+
 UserLovesListUI.PropTypes = {
   user:       PropTypes.object,
   loveAct:    PropTypes.array
 }
 
-const UserLovesList = createContainer ((props)=> {
-  let userId = props.user._id ? props.user._id : null
-  if(userId){
+const UserLovesList = createContainer( props => {
+  const userId = props.user._id ? props.user._id : null
+  if (userId) {
     let handleActivity = Meteor.subscribe("activity.public.recent.userId")
-    let selector = { byUserId: userId, activityType: "asset.userLoves" }
-    let findOpts = {sort : {timestamp: -1} }
+    let selector = {
+      byUserId: userId, 
+      activityType: "asset.userLoves" 
+    }
+    let findOpts = {sort : { timestamp: -1 } }
     return {
       loveAct: Activity.find(selector, findOpts).fetch(),
       loading: !handleActivity.ready()
-    };
+    }
   }
-}, UserLovesListUI);
+}, UserLovesListUI)
 export default UserLovesList

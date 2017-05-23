7c77e9aba0872309bd9c80dc51e4f64d23f53370
David Golds <david.p.golds@gmail.com>
2017-05-23 16:26:12 -0700

    UserLoves - handle some text/layout cases of 0 and 1 values

--- code13/app/client/imports/components/Controls/UserLoves.js
+++ code13/app/client/imports/components/Controls/UserLoves.js
@@ -1,11 +1,10 @@
 import _ from 'lodash'
 import React, { PropTypes } from 'react'
-import { Header, Icon, List, Popup } from 'semantic-ui-react'
+import { Icon, Popup } from 'semantic-ui-react'
 import style from './WorkState.css'
 import QLink from '/client/imports/routes/QLink'
 import { createContainer } from 'meteor/react-meteor-data'
-import { Users } from "/imports/schemas";
-
+import { Users } from "/imports/schemas"
 
 const UserLoveIcon = ( { size, onIconClick, currUserLoves } ) => (
   <Icon 
@@ -16,6 +15,7 @@ const UserLoveIcon = ( { size, onIconClick, currUserLoves } ) => (
       //className={`mgb-loves-${heartedBy}`} 
       />
 )
+
 const UserLovesUI = ( { seeLovers, userList, canEdit, size, popupPosition, onIconClick, currUserLoves, asset } ) => (
   <Popup
       on='hover'
@@ -24,7 +24,7 @@ const UserLovesUI = ( { seeLovers, userList, canEdit, size, popupPosition, onIco
       positioning={popupPosition}
       trigger={(
         <span>
-            <span>{asset.heartedBy_count}</span>
+          <small>{asset.heartedBy_count} </small>
           <UserLoveIcon 
               currUserLoves={currUserLoves} 
               size={size} 
@@ -33,43 +33,46 @@ const UserLovesUI = ( { seeLovers, userList, canEdit, size, popupPosition, onIco
       )} >
       { seeLovers && 
         <Popup.Header>
-            Users who love this
+          Users who love this
         </Popup.Header>
-        }
+      }
       <Popup.Content>
-      
-        { seeLovers ? userList.map((person, idx) => (
-            <div key={idx} >
-                <QLink to={`/u/${person.username}`}>
-                    <p>{person.username}</p>
-                </QLink>
-            </div>
-        )) : 
-        <QLink to={`/u/${asset.dn_ownerName}/asset/${asset._id}`}>
-            {asset.heartedBy ? asset.heartedBy.length : 0} loves 
-        </QLink>
+        { seeLovers ? 
+            ( userList.length > 0 ? 
+                userList.map((person, idx) => (
+                  <div key={idx} >
+                      <QLink to={`/u/${person.username}`}>
+                          <p>{person.username}</p>
+                      </QLink>
+                  </div>
+                ))
+              :
+                <span>(none yet)</span>
+            )
+          : 
+            <QLink to={`/u/${asset.dn_ownerName}/asset/${asset._id}`}>
+                {asset.heartedBy ? asset.heartedBy.length : 0} love{asset.heartedBy_count !== 1 ? 's' : ''}
+            </QLink>
         }
       </Popup.Content>
   </Popup>
 )
 
-
-UserLovesUI.propTypes = {     // E.g  "unknown"
+UserLovesUI.propTypes = {
   popupPosition:  PropTypes.string.isRequired,
-  handleChange:   PropTypes.func,                   // Callback function - single param is new workState string. Only called if CanEdit is true
   canEdit:        PropTypes.bool,
-  asset:       PropTypes.object,
+  asset:          PropTypes.object,
   onIconClick:    PropTypes.func,
-  currUserLoves: PropTypes.bool,
-  seeLovers:   PropTypes.bool
-                    // If false, then don't allow popup/change
+  currUserLoves:  PropTypes.bool,
+  seeLovers:      PropTypes.bool
 }
 UserLovesUI.defaultProps = {
   popupPosition: "bottom right",
 }
+
+
+
 const UserLoves = createContainer ((props)=> {
-  
-  
   const heartedByIds = props.asset.heartedBy
   const hasHearts = _.isArray(heartedByIds) && heartedByIds.length > 0
   const selector = {_id: {"$in": heartedByIds}}

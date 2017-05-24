66c59c217a743e56fcfac8d9edad28c50cdb42a1
David Golds <david.p.golds@gmail.com>
2017-05-23 21:50:25 -0700

    Browsegames - get rid of locked filter

--- code13/app/client/imports/routes/BrowseGamesRoute.js
+++ code13/app/client/imports/routes/BrowseGamesRoute.js
@@ -52,8 +52,8 @@ export default BrowseGamesRoute = React.createClass({
       newQ.sort = q.sort
     if (q.project)
       newQ.project = q.project
-    if (q.showStable === '0' || q.showStable === '1')
-      newQ.showStable = q.showStable
+    // if (q.showStable === '0' || q.showStable === '1')
+    //   newQ.showStable = q.showStable
     if (q.searchName)
       newQ.searchName = q.searchName
       
@@ -165,11 +165,15 @@ export default BrowseGamesRoute = React.createClass({
             &emsp;
           </div>
 
-          <div style={{ float: 'right' }}>
-            <AssetShowStableSelector 
-                showStableFlag={qN.showStable} 
-                handleChangeFlag={v => this._updateLocationQuery( { showStable: v } ) } />
-          </div>
+          { 
+            /* Show only locked games.. ? 
+            <div style={{ float: 'right' }}>
+              <AssetShowStableSelector 
+                  showStableFlag={qN.showStable} 
+                  handleChangeFlag={v => this._updateLocationQuery( { showStable: v } ) } />
+            </div>
+            */
+          }
 
         { user && 
           <div style={{clear: 'both'}}>

c0265664a12cba78df3e522404d5594c4c7d4fda
David Golds <david.p.golds@gmail.com>
2017-05-25 09:28:03 -0700

    goddam delete key

--- code13/app/client/imports/components/Assets/EditGraphic/MiniMap/MiniMap.js
+++ code13/app/client/imports/components/Assets/EditGraphic/MiniMap/MiniMap.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import ReactDOM from 'react-dom'
-import { Button } from 'semantic-ui-react
+import { Button } from 'semantic-ui-react'
 
 import sty from  '../editGraphic.css'
 

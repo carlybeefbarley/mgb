b3409e110f7cb1d524489d66c710c04de873052e
Guntis <guntis.smaukstelis@gmail.com>
2017-06-05 17:13:48 +0300

    fix #934 hotjar for landing page

--- code13/landing/src/js/main.js
+++ code13/landing/src/js/main.js
@@ -15,6 +15,21 @@ window.addEventListener( 'load', function () {
   }
 
   // ----------------------------------------
+  // Hotjar
+  // ----------------------------------------
+  function initHotjar(){
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:446876,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=')
+  }
+  setTimeout(initHotjar, 200)  
+
+  // ----------------------------------------
   // For Everyone Tabs
   // ----------------------------------------
   var $forEveryoneMenus = document.querySelectorAll( '.mgb-for-everyone-menu' )

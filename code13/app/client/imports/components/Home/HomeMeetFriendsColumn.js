e838eb3aaef5a1a60dbffb2bfc7e454ec6abf0ab
David Golds <david.p.golds@gmail.com>
2017-06-10 19:30:13 -0700

    homeMeetFriends - refact to use UX atoms and also fix sizing bug with user avatars

--- code13/app/client/imports/components/Home/HomeMeetFriendsColumn.js
+++ code13/app/client/imports/components/Home/HomeMeetFriendsColumn.js
@@ -1,8 +1,9 @@
 import React, { PropTypes } from 'react'
 import homeStyles from '/client/imports/routes/home.css'
 import getStartedStyle from '/client/imports/routes/GetStarted.css'
+import UX from '/client/imports/UX'
 
-import { Grid, Header, List, Icon, Image, Button } from 'semantic-ui-react'
+import { Grid, Header, List, Icon, Button } from 'semantic-ui-react'
 import QLink from '/client/imports/routes/QLink'
 import { createContainer } from 'meteor/react-meteor-data'
 import { Users } from '/imports/schemas'
@@ -16,11 +17,13 @@ const HomeMeetFriendsColumnUI = ( { loading, userList }) => (
         loading === false &&
         userList.map( (person, idx) => (
           <List.Item key={idx} as={QLink} to={`/u/${person.username}`}>
-            <Image verticalAlign='middle' style={{ height: 60, width: 60 }} src={person.profile.avatar} />
-            <div className="content middle aligned" style={{ marginLeft: '1em' }}>
+            <List.Content floated='left' style={{minWidth: '90px'}} >
+              <UX.UserAvatarNoLink username={person.username} height='60px' />
+            </List.Content>
+            <List.Content verticalAlign='middle' style={{ marginLeft: '1em' }}>
               <Header as='h3' content={person.username} />
               <p><Icon color='yellow' name='trophy' />{person.badges ? person.badges.length : 0} badges</p>
-            </div>
+            </List.Content>
           </List.Item>
         ))
       }

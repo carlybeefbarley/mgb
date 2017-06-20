f81fe0bb3654f1a4a710c0d636b4d6555c875ee7
Kaspars Stauzs <stauzs@gmail.com>
2017-06-20 12:40:47 +0300

    save meta changes - Map Proeperties refactor WIP

--- code13/app/client/imports/components/Assets/EditMap/EditMap.js
+++ code13/app/client/imports/components/Assets/EditMap/EditMap.js
@@ -395,7 +395,7 @@ export default class EditMap extends React.Component {
   }
 
   _saveMeta(){
-    this.props.handleMetadataChange(this.props.asset.metadata)
+    this.props.handleMetadataChange(this.mgb_meta)
   }
   // probably copy of data would be better to hold .. or not research strings vs objects
   // TODO(stauzs): research memory usage - strings vs JS objects

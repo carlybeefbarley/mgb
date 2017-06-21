516e0db9646e5c7ead2003fbe4f70605c3f84be3
David Golds <david.p.golds@gmail.com>
2017-06-21 10:47:00 -0700

    EditCode: Added collapsible help text for ImportAssistant

--- code13/app/client/imports/components/Assets/EditCode/tern/ImportHelperPanel.js
+++ code13/app/client/imports/components/Assets/EditCode/tern/ImportHelperPanel.js
@@ -70,78 +70,79 @@ export default class ImportHelperPanel extends React.Component {
 
     return (
       <Segment color='blue' style={{backgroundColor: "rgba(160,32,240,0.03)", color: "#2A00FF"}}>
-
+        { !!showExpanded &&
+          <p style={{color: '#333'}}>
+            The <a href='https://github.com/lukehoban/es6features#readme' target='_blank'>ES6</a> <strong><code>import</code></strong> statement
+            is used to import JavaScript functions, objects or primitives that have
+            been <code>export</code>ed from another code file or package. This UI
+            tool helps new users import useful packages, or from code they have written.
+          </p>
+        }
         <Label
             color='blue'
             corner='right'
-            title={`Click to ${ showExpanded ? "hide" : "show" } import helper`}
+            title={`Click to ${ showExpanded ? "hide" : "show" } explanation`}
             onClick={this.handleHideShowClick}
             icon={showExpanded ? "minus circle" : "add circle"} />
 
-        {!!showExpanded &&
-        <div>
-          <Label ribbon content={<small>Recommended JavaScript packages to <code>import</code></small>} />
 
-          <div style={{margin: '5px 0'}}>
-            <Dropdown
-              fluid search selection
-              options={ this.popular }
-              placeholder='Select package from list to import it'
-              value=''
-              selectOnBlur={false}
-              onChange={ (a, b) => { includeExternalImport(settings.editCode.popularLibs[b.value]) }}
-              />
-          </div>
+        <Label ribbon content={<span>Recommended packages to <code>import</code></span>} />
 
-
-          <Label ribbon content={<small>My Scripts to <code>import</code></small>} />
-
-          <div style={{margin: '5px 0'}}>
-            <Dropdown
-              fluid search selection
-              options={ this.options }
-              placeholder='Select script from list to import it'
-              value=''
-              selectOnBlur={false}
-              onChange={ (a, b) => { includeLocalImport(b.value) }}
+        <div style={{margin: '5px 0'}}>
+          <Dropdown
+            fluid search selection
+            options={ this.popular }
+            placeholder='Select a package to import'
+            value=''
+            selectOnBlur={false}
+            onChange={ (a, b) => { includeExternalImport(settings.editCode.popularLibs[b.value]) }}
             />
-          </div>
-
-
-          <div>
-            <small>
-              <Icon name='info circle' color='black'/>
-              <span className="cm-s-eclipse">You can <code className="cm-keyword">import</code> other user's assets, e.g:
-                <br />
-                <code>
-                  <span className="cm-keyword">import</span>&nbsp;
-                  <span className="cm-def">otherModule</span>&nbsp;
-                  <span className="cm-keyword">from</span>&nbsp;
-                  <span className="cm-string">'/!vault:CSSLoader'</span>
-                </code>
-              </span>
-            </small>
-          </div>
-          <div>
-            <small>
-              <Icon name='info circle' color='black'/>
-              <span className="cm-s-eclipse">
-                Hit Ctrl + Space to get list of user assets after typing '/username:'
-                <br />
-                <code>
-                  <span className="cm-keyword">import</span>&nbsp;
-                  <span className="cm-def">otherModule</span>&nbsp;
-                  <span className="cm-keyword">from</span>&nbsp;
-                  <span className="cm-string">'/!vault:<span className="blink">|</span>'</span>
-                </code>
-              </span>
-            </small>
-          </div>
         </div>
-        }
-        { !showExpanded &&
-         <Label color='blue' ribbon content={<small><code>import</code> helper</small>} />
-        }
+
+
+        <Label ribbon content={<span>My Code to <code>import</code></span>} />
+
+        <div style={{margin: '5px 0'}}>
+          <Dropdown
+            fluid search selection
+            options={ this.options }
+            placeholder='Select Code to import'
+            value=''
+            selectOnBlur={false}
+            onChange={ (a, b) => { includeLocalImport(b.value) }}
+          />
+        </div>
+
+
+        <div>
+          <small>
+            <Icon name='info circle' color='black'/>
+            <span className="cm-s-eclipse">You can <code className="cm-keyword">import</code> other user's assets, e.g:
+              <br />
+              <code>
+                <span className="cm-keyword">import</span>&nbsp;
+                <span className="cm-def">otherModule</span>&nbsp;
+                <span className="cm-keyword">from</span>&nbsp;
+                <span className="cm-string">'/!vault:CSSLoader'</span>
+              </code>
+            </span>
+          </small>
+        </div>
+        <div>
+          <small>
+            <Icon name='info circle' color='black'/>
+            <span className="cm-s-eclipse">
+              Hit Ctrl + Space to get list of user assets after typing '/username:'
+              <br />
+              <code>
+                <span className="cm-keyword">import</span>&nbsp;
+                <span className="cm-def">otherModule</span>&nbsp;
+                <span className="cm-keyword">from</span>&nbsp;
+                <span className="cm-string">'/!vault:<span className="blink">|</span>'</span>
+              </code>
+            </span>
+          </small>
+        </div>
       </Segment>
     )
   }

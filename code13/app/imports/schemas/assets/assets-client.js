2f9fd4e328d6d8be19b505b778524a1fa2b19ba2
David Golds <david.p.golds@gmail.com>
2017-06-22 13:41:57 -0700

    add support for breadcrumb name

--- code13/app/imports/schemas/assets/assets-client.js
+++ code13/app/imports/schemas/assets/assets-client.js
@@ -3,14 +3,15 @@
 /**
  * Simple helper to duplicate some nav-related info about an Asset.
  * The primary use case for this is how some pages that have an asset focus
- * (e.g. asset editors, play Game etc) will pass this info up to App.js so it 
+ * (e.g. asset editors, play Game etc) will pass this info up to App.js so it
  * can be used to provide nav context in Breadcrumb bars, etc
- * 
- * @param {object} asset 
+ *
+ * @param {object} asset
  * @param {string} assetVerb.. e.g 'View', 'Edit', 'Play' etc
  */
-export const makeAssetInfoFromAsset = (asset, assetVerb) => ( { 
-  kind: asset.kind, 
+export const makeAssetInfoFromAsset = (asset, assetVerb) => ( {
+  kind: asset.kind,
+  name: asset.name,
   assetVerb: assetVerb,
   isDeleted: asset.isDeleted,
   isLocked: asset.isCompleted,

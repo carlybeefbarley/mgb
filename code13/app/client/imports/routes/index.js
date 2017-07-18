55c138a31c923e60da66dd092501b32e3b56277f
Kaspars Stauzs <stauzs@gmail.com>
2017-07-18 18:03:24 +0300

    clean up routes

--- code13/app/client/imports/routes/index.js
+++ code13/app/client/imports/routes/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Callum from './Callum'
-
-import Test from './Test'
 
 import { Router, Route, browserHistory } from 'react-router'
 import urlMaker from './urlMaker'
--- code13/app/lint.js
+++ code13/app/lint.js
@@ -4,16 +4,22 @@ const isCI = process.env.CI === 'true'
 
 const include = ['**/*.js?(on|x)']
 const ignore = ['**/lib/**', '.meteor/**', '.yarn-cache/**', 'public/phaser/**']
+try {
+  runNPMCommand('prettier', [
+    isCI ? '--list-different' : '--write',
+    ...include.map(glob => `"${glob}"`),
+    ...ignore.map(glob => `"!${glob}"`),
+  ])
 
-runNPMCommand('prettier', [
-  isCI ? '--list-different' : '--write',
-  ...include.map(glob => `"${glob}"`),
-  ...ignore.map(glob => `"!${glob}"`),
-])
+  console.log('Linting...')
+  runNPMCommand('eslint', [
+    !isCI && '--fix',
+    ...include.map(glob => `"${glob}"`),
+    ...ignore.map(glob => `--ignore-pattern "${glob}"`),
+  ])
 
-console.log('Linting...')
-runNPMCommand('eslint', [
-  !isCI && '--fix',
-  ...include.map(glob => `"${glob}"`),
-  ...ignore.map(glob => `--ignore-pattern "${glob}"`),
-])
+}
+// TODO: @levithomason fix this somehow.. seems meteor startup gets wrong path in the runNPMCommand
+catch(e){
+  console.log("Meteor build sideffect", e)
+}

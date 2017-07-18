b5ec6f22cdedf38602ea96c5dee97e93a654c28d
Guntis <guntis.smaukstelis@gmail.com>
2017-07-18 15:43:11 +0300

    lint workaround for linux

--- code13/app/lint.js
+++ code13/app/lint.js
@@ -1,19 +1,22 @@
 const runNPMCommand = require('./run-npm-command')
 
-const isCI = process.env.CI === 'true'
+// guntis: workaround for linux
+const x = () => {
+  const isCI = process.env.CI === 'true'
 
-const include = ['**/*.js?(on|x)']
-const ignore = ['**/lib/**', '.meteor/**', '.yarn-cache/**', 'public/phaser/**']
+  const include = ['**/*.js?(on|x)']
+  const ignore = ['**/lib/**', '.meteor/**', '.yarn-cache/**', 'public/phaser/**']
 
-runNPMCommand('prettier', [
-  isCI ? '--list-different' : '--write',
-  ...include.map(glob => `"${glob}"`),
-  ...ignore.map(glob => `"!${glob}"`),
-])
+  runNPMCommand('prettier', [
+    isCI ? '--list-different' : '--write',
+    ...include.map(glob => `"${glob}"`),
+    ...ignore.map(glob => `"!${glob}"`),
+  ])
 
-console.log('Linting...')
-runNPMCommand('eslint', [
-  !isCI && '--fix',
-  ...include.map(glob => `"${glob}"`),
-  ...ignore.map(glob => `--ignore-pattern "${glob}"`),
-])
+  console.log('Linting...')
+  runNPMCommand('eslint', [
+    !isCI && '--fix',
+    ...include.map(glob => `"${glob}"`),
+    ...ignore.map(glob => `--ignore-pattern "${glob}"`),
+  ])
+}

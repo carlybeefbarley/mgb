757aa8b76e09d61fc04a6a906b60c8f831fc5955
Kaspars Stauzs <stauzs@gmail.com>
2017-07-25 15:03:52 +0300

    Reset map on errors: fix #1195

--- code13/app/client/imports/components/Assets/EditMap/EditMap.js
+++ code13/app/client/imports/components/Assets/EditMap/EditMap.js
@@ -140,8 +140,17 @@ export default class EditMap extends React.Component {
     return this.refs.map.generatePreview()
   }
 
+  isMapBroken(){
+    if(Object.keys(this.props.asset.content2).length === 0)
+      return true
+    const c2 = this.props.asset.content2
+    if(typeof c2.layers !== 'object')
+      return true
+
+    
+  }
   setInitialStateFromContent() {
-    if (Object.keys(this.props.asset.content2).length === 0) {
+    if (this.isMapBroken()) {
       this.createNewMap()
       return
     }

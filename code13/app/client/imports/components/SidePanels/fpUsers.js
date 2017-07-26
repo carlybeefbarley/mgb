7cd1c6448a25bb8346fef41ff0044b98786537d5
David Golds <david.p.golds@gmail.com>
2017-07-26 02:48:54 -0500

    fix #1206. flexPanel user list now works again (#1209)

--- code13/app/client/imports/components/SidePanels/fpUsers.js
+++ code13/app/client/imports/components/SidePanels/fpUsers.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import UserListRoute from '/client/imports/routes/Users/UserListRoute'
 
-const fpUsers = () => <UserListRoute initialLimit={20} renderVertical />
+const fpUsers = ({ location }) => <UserListRoute initialLimit={20} renderVertical location={location} />
 
 export default fpUsers

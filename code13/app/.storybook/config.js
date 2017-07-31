202f73e1cfbe47d022ed7bcaffe49f5321199482
Levi Thomason <me@levithomason.com>
2017-07-31 15:31:26 -0700

    chore(storybook): update storybook and setup jest

--- code13/app/.storybook/config.js
+++ code13/app/.storybook/config.js
@@ -1,8 +1,43 @@
-import { configure } from '@kadira/storybook'
+import { addDecorator, configure } from '@storybook/react'
+import { withKnobsOptions } from '@storybook/addon-knobs'
+import { setOptions } from '@storybook/addon-options'
 
+// ----------------------------------------
+// Addons
+// ----------------------------------------
+
+// Options
+setOptions({
+  name: 'My Game Builder',
+  url: 'http://build.games',
+  goFullScreen: false,
+  showLeftPanel: true,
+  showDownPanel: true,
+  showSearchBox: false,
+  downPanelInRight: true,
+  sortStoriesByKind: true,
+})
+
+// Knobs
+addDecorator(
+  withKnobsOptions({
+    // Same as lodash debounce
+    debounce: { wait: '100', leading: false },
+    // Don't emit events while user is typing
+    timestamps: true,
+  }),
+)
+
+// ----------------------------------------
+// Stories
+// ----------------------------------------
 function loadStories() {
-  require('../client/imports/__stories.js')
-  // You can require as many stories as you need.
+  // dynamically load all *.stories.js files in /client/imports
+  const storyCtx = require.context('../client/imports', true, /__stories__\/.*\.stories\.js$/)
+  storyCtx.keys().forEach(storyCtx)
+
+  // load the example stories
+  require('./__stories__/Example.stories')
 }
 
 configure(loadStories, module)

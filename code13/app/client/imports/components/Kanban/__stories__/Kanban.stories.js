30f4b92f36a764c29e0021e2b98edb8b6ea16c94
David Zukowski <zukoweb@gmail.com>
2017-09-17 11:12:14 -0500

    wip: mock card removal

--- code13/app/client/imports/components/Kanban/__stories__/Kanban.stories.js
+++ code13/app/client/imports/components/Kanban/__stories__/Kanban.stories.js
@@ -2,7 +2,7 @@
 
 SPEC
 
-* [ ] Default columns for board (todo, in progress, done)
+* [ ] Add/Remove columns
 * [ ] Add/Remove cards (UI is via column)
 * [ ] Order cards within column
 * [ ] Move cards between columns
@@ -10,14 +10,14 @@ SPEC
 * [ ] Labelled cards
 *  ^---- board is associated w/ group of labels
 * [ ] Boards have a default set of labels (bug, feature, help wanted)
-* [ ] Example todo card, changes title as it moves between columns. Tutorial card.
-* [ ] Add/Remove columns
+* [ ] Default columns for board (todo, in progress, done)
 * [ ] Set column title
 * [ ] Reorder columns (global)
 * [ ] Column shows how many cards it has in title
 * [ ] Search for cards (searches by titles, at least)
 * [ ] Dropdown/multiselect? for assignee
 * [ ] Dropdown/multiselect? for label
+* [ ] Example todo card, changes title as it moves between columns. Tutorial card.
 */
 
 import React from 'react'
@@ -25,7 +25,7 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import * as knobs from '@storybook/addon-knobs'
 import { linkTo } from '@storybook/addon-links'
-import { Button, Card, Container, Image, Segment } from 'semantic-ui-react'
+import { Button, Card, Container, Image, Modal, Segment } from 'semantic-ui-react'
 
 const stories = storiesOf('Kanban', module)
 
@@ -34,7 +34,14 @@ class KanbanBoard extends React.Component {
     return (
       <div>
         {this.props.columns.map(col => (
-          <KanbanColumn title={col.title} cards={col.cards} />
+          <KanbanColumn
+            key={col.id}
+            id={col.id}
+            title={col.title}
+            cards={col.cards}
+            onAddCard={this.props.onAddCard}
+            onRemoveCard={this.props.onRemoveCard}
+          />
         ))}
       </div>
     )
@@ -46,14 +53,22 @@ SUI Components:
  - Header
 */
 class KanbanColumn extends React.Component {
+  _onRemoveCard = (cardId) => {
+    this.props.onRemoveCard(this.props.id, cardId)
+  }
+
   render () {
     const { cards, title } = this.props
     return (
       <div>
         <h2>{title}</h2>
         <hr />
-        {cards.map(item => (
-          <KanbanCard {...item} />
+        {cards.map(card => (
+          <KanbanCard
+            key={card.id}
+            {...card}
+            onRemove={this._onRemoveCard}
+          />
         ))}
       </div>
     )
@@ -67,6 +82,11 @@ SUI Components:
   - UserAvatar (but wont be able to use real component due to meteor) -- Image w/ avatar prop
 */
 class KanbanCard extends React.Component {
+  _onRemove = (e) => {
+    e.preventDefault()
+    this.props.onRemove(this.props.id)
+  }
+
   render () {
     const { title, description, assigned } = this.props
     return (
@@ -75,6 +95,9 @@ class KanbanCard extends React.Component {
           <Card.Content>
             <Card.Header>
               {title}
+              <div style={{ float: 'right' }} onClick={this._onRemove}>
+                &times;
+              </div>
             </Card.Header>
             <Card.Meta>
               {description}
@@ -89,21 +112,73 @@ class KanbanCard extends React.Component {
   }
 }
 
-const board = [
-  { title: 'icebox', cards: [
-    { title: 'Something Iceboxed', description: 'foobar' },
-    { title: 'Another Thing Iceboxed', description: 'foobar' },
-  ]},
-  { title: 'current', cards: [
-    { title: 'Something Current', description: 'foobar' },
-    { title: 'Another Thing Current', description: 'foobar' },
-    { title: 'Another Current Card', description: 'foobar' },
-  ]},
-]
+
+const uuid = ((seed) => () => ++seed)(0)
+
+const createColumn = (title, cards) => ({
+  id: uuid(),
+  title,
+  cards: cards.map(createCard),
+})
+
+const createCard = (data) => ({
+  id: uuid(),
+  ...data,
+})
+
+class MockKanbanContainer extends React.Component {
+  state = {
+    board: [
+      createColumn('icebox', [
+        { title: 'Something Iceboxed', description: 'foobar' },
+        { title: 'Another Thing Iceboxed', description: 'foobar' },
+      ]),
+      createColumn('current', [
+        { title: 'Something Current', description: 'foobar' },
+        { title: 'Another Thing Current', description: 'foobar' },
+        { title: 'Another Current Card', description: 'foobar' },
+      ]),
+    ],
+  }
+
+  _onAddCard = (colId, card) => {
+    this.setState(state => ({
+      board: state.board.map(col => {
+        if (col.id !== colId) return col
+        return {
+          ...col,
+          cards: [...col.cards, createCard(card)],
+        }
+      })
+    }))
+  }
+
+  _onRemoveCard = (colId, cardId) => {
+    this.setState(state => ({
+      board: state.board.map(col => {
+        if (col.id !== colId) return col
+        return {
+          ...col,
+          cards: col.cards.filter(c => c.id !== cardId),
+        }
+      })
+    }))
+  }
+
+  render () {
+    return (
+      <KanbanBoard
+        columns={this.state.board}
+        onAddCard={this._onAddCard}
+        onRemoveCard={this._onRemoveCard}
+      />
+    )
+  }
+}
 
 stories.add('Test', () => (
   <Container text>
-    <KanbanBoard columns={board} />
+    <MockKanbanContainer />
   </Container>
 ))
 

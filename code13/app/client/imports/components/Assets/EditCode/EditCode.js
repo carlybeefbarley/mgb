d4edbbd893a9b4f0a14cfaf7659edf1c25c64d7d
Brian Pak <bbouhm@gmail.com>
2017-11-21 10:18:07 -0800

    re-enable 1 hour time limit (#1624)

--- code13/app/client/imports/components/Assets/EditCode/EditCode.js
+++ code13/app/client/imports/components/Assets/EditCode/EditCode.js
@@ -388,9 +388,7 @@ class EditCode extends React.Component {
         }
       }, 200)
 
-      /*
       // Check every second for activity time expiration
-      // DISABLED FOR TESTING; UNCOMMENT FOR HOC
       var activityTimerId = window.setInterval(() => {
         if (!this.props.hourOfCodeStore.state.isActivityOver) {
           this.props.hourOfCodeStore.checkActivityTime()
@@ -398,7 +396,6 @@ class EditCode extends React.Component {
           window.clearInterval(activityTimerId)
         }
       }, 10 * 1000)
-      */
     }
   }
 

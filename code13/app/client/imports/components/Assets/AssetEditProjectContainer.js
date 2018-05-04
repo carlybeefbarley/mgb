2e20768d0803bcb29f25b8f3b5ef47495a85c21e
Brandon Grissom <lousaydus@gmail.com>
2018-05-03 18:24:16 -0700

    Converted AssetEditProjectContainer to stateless component.

--- code13/app/client/imports/components/Assets/AssetEditProjectContainer.js
+++ code13/app/client/imports/components/Assets/AssetEditProjectContainer.js
@@ -1,3 +1,11 @@
+/*
+ *************************************************************
+ * This component wraps the asset editor to provide IDE-like options
+ * such as creating new assets, tabs for each open asset, and quick project
+ * swapping.
+ *************************************************************
+ */
+
 import React from 'react'
 import _ from 'lodash'
 import { Grid, Dropdown, Button, Segment, Icon } from 'semantic-ui-react'
@@ -5,78 +13,77 @@ import AssetCreateNewModal from '/client/imports/components/Assets/NewAsset/Asse
 import RelatedAssets from '/client/imports/components/Nav/RelatedAssets'
 import QLink, { utilPushTo } from '/client/imports/routes/QLink'
 
-export default class AssetEditProjectContainer extends React.Component {
-  render() {
-    const { currUserProjects, currUser, currentlyEditingAssetInfo, params, user } = this.props
-    let projectName = _.first(currentlyEditingAssetInfo.projectNames)
-    return (
-      <div>
-        <Grid padded columns="equal" style={{ flex: '0 0 auto' }}>
-          <Grid.Column style={{ flex: '0 0 20em' }}>
-            <div style={{ display: 'inline', fontSize: '2em' }}>
-              <Dropdown
-                selectOnBlur={false}
-                selectOnNavigation={false}
-                button
-                className="basic secondary right labeled icon"
-                fluid
-                search
-                scrolling
-                value={projectName}
-                text={
-                  <span>
-                    <Icon name="sitemap" /> {projectName}
-                  </span>
-                }
-                onChange={(e, { value }) => {
-                  const project = _.find(currUserProjects, { name: value })
-                  utilPushTo(location.query, `/u/${project.ownerName}/projects/${project.name}`)
-                }}
-                options={_.map(currUserProjects, project => ({
-                  key: project.name,
-                  text: project.name,
-                  value: project.name,
-                  icon: 'sitemap',
-                }))}
-              />
-            </div>
-          </Grid.Column>
-          <Grid.Column>
-            <AssetCreateNewModal
+export default function AssetEditProjectContainer(props) {
+  const { currUserProjects, currUser, currentlyEditingAssetInfo, params, user } = props
+  // Set the name for the projects dropdown list
+  const projectName = _.first(currentlyEditingAssetInfo.projectNames)
+  return (
+    <div>
+      <Grid padded columns="equal" style={{ flex: '0 0 auto' }}>
+        <Grid.Column style={{ flex: '0 0 20em' }}>
+          <div style={{ display: 'inline', fontSize: '2em' }}>
+            <Dropdown
+              selectOnBlur={false}
+              selectOnNavigation={false}
+              button
+              className="basic secondary right labeled icon"
+              fluid
+              search
+              scrolling
+              value={projectName}
+              text={
+                <span>
+                  <Icon name="sitemap" /> {projectName}
+                </span>
+              }
+              onChange={(e, { value }) => {
+                const project = _.find(currUserProjects, { name: value })
+                utilPushTo(location.query, `/u/${project.ownerName}/projects/${project.name}`)
+              }}
+              options={_.map(currUserProjects, project => ({
+                key: project.name,
+                text: project.name,
+                value: project.name,
+                icon: 'sitemap',
+              }))}
+            />
+          </div>
+        </Grid.Column>
+        <Grid.Column>
+          <AssetCreateNewModal
+            currUser={currUser}
+            currUserProjects={currUserProjects}
+            buttonProps={{ floated: 'right' }}
+            viewProps={{
+              showProjectSelector: false,
+              suggestedParams: { projectName },
+            }}
+          />
+          <Button
+            as={QLink}
+            floated="right"
+            to={`/u/${currentlyEditingAssetInfo.ownerName}/projects/${projectName}`}
+          >
+            Project Overview
+          </Button>
+        </Grid.Column>
+      </Grid>
+      <Grid padded columns="equal" style={{ flex: '1' }}>
+        <Grid.Column stretched style={{ flex: '0 0 20em', overflowY: 'auto' }}>
+          <Segment>
+            <RelatedAssets
+              projectName={projectName}
+              location={location}
+              user={user}
               currUser={currUser}
               currUserProjects={currUserProjects}
-              buttonProps={{ floated: 'right' }}
-              viewProps={{
-                showProjectSelector: false,
-                suggestedParams: { projectName },
-              }}
+              params={params}
+              currentlyEditingAssetInfo={currentlyEditingAssetInfo}
             />
-            <Button
-              as={QLink}
-              floated="right"
-              to={`/u/${currentlyEditingAssetInfo.ownerName}/projects/${projectName}`}
-            >
-              Project Overview
-            </Button>
-          </Grid.Column>
-        </Grid>
-        <Grid padded columns="equal" style={{ flex: '1' }}>
-          <Grid.Column stretched style={{ flex: '0 0 20em', overflowY: 'auto' }}>
-            <Segment>
-              <RelatedAssets
-                projectName={projectName}
-                location={location}
-                user={user}
-                currUser={currUser}
-                currUserProjects={currUserProjects}
-                params={params}
-                currentlyEditingAssetInfo={currentlyEditingAssetInfo}
-              />
-            </Segment>
-          </Grid.Column>
-          <Grid.Column style={{ overflowY: 'auto' }}>{this.props.children}</Grid.Column>
-        </Grid>
-      </div>
-    )
-  }
+          </Segment>
+        </Grid.Column>
+        <Grid.Column style={{ overflowY: 'auto' }}>{props.children}</Grid.Column>
+      </Grid>
+    </div>
+  )
 }

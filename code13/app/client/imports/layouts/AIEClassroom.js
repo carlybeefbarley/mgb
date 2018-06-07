a4ff114eda794038d3e435f043c74f230d989aac
Carly Coms <carly.d.coms@gmail.com>
2018-06-07 15:45:10 -0700

    Upcoming and Past Assignments Lists in classroom dashboard

--- code13/app/client/imports/layouts/AIEClassroom.js
+++ code13/app/client/imports/layouts/AIEClassroom.js
@@ -90,6 +90,87 @@ class AssignmentsList extends React.Component {
   }
 }
 
+class UpcomingClassAssignmentsList extends React.Component {
+  render() {
+    return (
+      <List>
+        <List.Item key="id">
+          <List.Icon name="student" />
+          <List.Content style={{ width: '100%' }}>
+            <List.Content floated="right">
+              <small style={{ color: 'lightgray' }}>when</small>
+            </List.Content>
+            <List.Header>Foo bar</List.Header>
+            <List.Description>
+              <small>Carly is cool</small>
+            </List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item key="id2">
+          <List.Icon name="code" />
+          <List.Content style={{ width: '100%' }}>
+            <List.Content floated="right">
+              <small style={{ color: 'lightgray' }}>when</small>
+            </List.Content>
+            <List.Header>Your Mom</List.Header>
+            <List.Description>
+              <small>goes to college</small>
+            </List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item key="id3">
+          <List.Icon name="clock" />
+          <List.Content style={{ width: '100%' }}>
+            <List.Content floated="right">
+              <small style={{ color: 'lightgray' }}>when</small>
+            </List.Content>
+            <List.Header>Llama Llama</List.Header>
+            <List.Description>
+              <small>red pajamas</small>
+            </List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item key="id4">
+          <List.Icon name="keyboard" />
+          <List.Content style={{ width: '100%' }}>
+            <List.Content floated="right">
+              <small style={{ color: 'lightgray' }}>when</small>
+            </List.Content>
+            <List.Header>Bread Jedi</List.Header>
+            <List.Description>
+              <small>flexible bean</small>
+            </List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item key="id5">
+          <List.Icon name="smile" />
+          <List.Content style={{ width: '100%' }}>
+            <List.Content floated="right">
+              <small style={{ color: 'lightgray' }}>when</small>
+            </List.Content>
+            <List.Header>See ya See ya</List.Header>
+            <List.Description>
+              <small>wouldn't wanna be ya</small>
+            </List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item key="id6">
+          <List.Icon name="female" />
+          <List.Content style={{ width: '100%' }}>
+            <List.Content floated="right">
+              <small style={{ color: 'lightgray' }}>when</small>
+            </List.Content>
+            <List.Header>Girls Rule</List.Header>
+            <List.Description>
+              <small>boys drool</small>
+            </List.Description>
+          </List.Content>
+        </List.Item>
+      </List>
+    )
+  }
+}
+
 class Dashboard extends React.Component {
   static propTypes = {
     currUser: PropTypes.object,
@@ -255,8 +336,18 @@ class ClassroomDashboard extends React.Component {
             </Grid.Column>
           </Grid.Row>
         </Grid>
-
-        <Divider hidden />
+        <Segment raised color="yellow">
+          <Grid columns={2} padded stackable>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <UpcomingClassAssignmentsList />
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <UpcomingClassAssignmentsList />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
       </div>
     )
   }

dedc82fa698e2d2634d074a49ddb13c7e5700fdf
Hudson <lousaydus@gmail.com>
2018-06-11 17:56:00 -0700

    Aie deploy scripts/test scripts (#1759)
    * update deploy script
    
    * remove unneeded file
    
    * echo hardcoded aie
    
    * Started new scripts for deployment to AIE and support for DB endpoints per deploy target.
    
    * Fixed error in endpoint resolver script
    
    * More fixes to deployment script
    
    * Testing deployment to AIE
    
    * Scripts now working properly but mlab container init fails. Probably due to something with the oplog setting in resolvedbendpoints.sh.
    
    * Okay the deploy scripts are working for realsies this time. Tested deploy with success to AIE subdomain.
    
    * Added: aie start script. "npm start start:aie"
    
    * Fixed: Linting error with static assignment of boolean to JSX value.

--- code13/app/client/imports/layouts/AIEClassroom.js
+++ code13/app/client/imports/layouts/AIEClassroom.js
@@ -136,7 +136,7 @@ class Dashboard extends React.Component {
                   maxWidth="auto"
                   imageSrc={avatar}
                   header="User Avatar"
-                  canEdit={true}
+                  canEdit
                 />
                 <Container style={infoStyle}>
                   <p>

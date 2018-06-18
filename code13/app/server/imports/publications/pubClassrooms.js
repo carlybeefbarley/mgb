0de2ac9f77dace57750b32eb8bc6dcf844609d2d
Hudson Mercer <hudson@mycodebuilder.com>
2018-06-18 11:10:35 -0700

    Added: Subscription for classrooms by Id

--- code13/app/server/imports/publications/pubClassrooms.js
+++ code13/app/server/imports/publications/pubClassrooms.js
@@ -1,7 +1,18 @@
+import _ from 'lodash'
 import { Classrooms } from '/imports/schemas'
 
-Meteor.publish('classrooms.byUserId', sel => {
-  return Classrooms.find({ ownerId: sel, teacherIds: sel, studentIds: sel, assignmentAssetIds: sel }).fetch()
+Meteor.publish('classrooms.byUserId', function(userId) {
+  // const byOwner = Classrooms.find({ ownerId: userId }),
+  //   byTeacherId = Classrooms.find({ teacherIds: userId }),
+  //   byStudentId = Classrooms.find({ studentIds: userId }),
+  const target = Classrooms.find({
+    $or: [{ ownerId: userId }, { teacherIds: userId }, { studentIds: userId }],
+  })
+
+  // let returnArray = _.union(_.union(byOwner, byTeacherId), byStudentId)
+
+  // console.log(target)
+  return target
   // return Classrooms.find({ _id: sel })
 })
 

dad8bd02840cb00ad873d585d271235836e2c993
Hudson Mercer <hudson@mycodebuilder.com>
2018-07-02 13:25:51 -0700

    Fixed: Dev Panel teacher signup

--- code13/app/client/imports/routes/Dev/DevPanelRoute.js
+++ code13/app/client/imports/routes/Dev/DevPanelRoute.js
@@ -15,7 +15,7 @@ const mascotColumnStyle = {
   pointerEvents: 'none',
 }
 
-const title = [
+const titles = [
   { key: 0, text: 'Mr.', value: 0 },
   { key: 1, text: 'Mrs.', value: 1 },
   { key: 2, text: 'Ms.', value: 2 },
@@ -55,7 +55,7 @@ export default class DevPanelRoute extends Component {
     const { titleIndex } = this.state
     const teacherErrors = this.state.errors.teacher
     const studentErrors = this.state.errors.student
-    const title = title[titleIndex].text
+    const title = titles[titleIndex].text
 
     const errors = {
       email: validate.emailWithReason(email),
@@ -162,7 +162,7 @@ export default class DevPanelRoute extends Component {
   }
 
   handleSelect = event => {
-    const titleIndex = _.find(title, { text: event.target.innerText })
+    const titleIndex = _.find(titles, { text: event.target.innerText })
 
     if (titleIndex !== undefined) {
       this.setState({ titleIndex: titleIndex.value })
@@ -209,7 +209,7 @@ export default class DevPanelRoute extends Component {
                           label={'Title'}
                           name="title"
                           value={this.state.titleIndex}
-                          options={title}
+                          options={titles}
                           onChange={e => this.handleSelect(e)}
                         />
                         <Form.Input
@@ -218,7 +218,7 @@ export default class DevPanelRoute extends Component {
                           label={errors.teacher.username || 'Username (used for profile)'}
                           name="username"
                           onBlur={this.checkUserName}
-                          placeholder={`${title[this.state.titleIndex].text} Woodstock`}
+                          placeholder={`${titles[this.state.titleIndex].text} Woodstock`}
                         />
                         <Button
                           fluid
@@ -269,7 +269,7 @@ export default class DevPanelRoute extends Component {
                           label={errors.student.username || 'Username (used for profile)'}
                           name="username"
                           onBlur={this.checkUserName}
-                          placeholder={`${title[this.state.titleIndex].text} Woodstock`}
+                          placeholder={`${titles[this.state.titleIndex].text} Woodstock`}
                         />
                         <Button
                           fluid

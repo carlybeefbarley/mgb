39ad29a5c254182f3cb1d7a3f1a984d9701ffc7f
Hudson Mercer <hudson@mycodebuilder.com>
2018-07-06 09:56:16 -0700

    Fix render bug with Asset Edit Project Layout.

--- code13/app/client/imports/layouts/AssetEditProjectLayout.js
+++ code13/app/client/imports/layouts/AssetEditProjectLayout.js
@@ -55,7 +55,7 @@ export default class AssetEditProjectContainer extends React.Component {
 
   render() {
     const { currUserProjects, currUser, currentlyEditingAssetInfo, params, assetStore } = this.props
-    const user = currentlyEditingAssetInfo.ownerName || this.props.user.username
+    const user = currentlyEditingAssetInfo.ownerName
     const renderedProjectsList = this.renderProjectsList(currUserProjects)
     // Set the default name/option for the projects dropdown list
     const projectName = assetStore.project() || __NO_PROJECT__

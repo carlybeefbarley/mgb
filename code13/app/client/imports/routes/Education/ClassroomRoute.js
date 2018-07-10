cbf814885688209e2a6ff39b2853a833fa13af5b
Carly Coms <carly.d.coms@gmail.com>
2018-07-10 09:23:05 -0700

    scrollable assignment table

--- code13/app/client/imports/routes/Education/ClassroomRoute.js
+++ code13/app/client/imports/routes/Education/ClassroomRoute.js
@@ -63,20 +63,6 @@ class TeacherClassroomView extends React.Component {
             </Table.Row>
           </Table.Header>
           <Table.Body>{this.renderStudentRows()}</Table.Body>
-          {/* <Table.Footer>
-          <Menu floated="right" pagination>
-            <Menu.Item as="a" icon>
-              <Icon name="chevron left" />
-            </Menu.Item>
-            <Menu.Item as="a">1</Menu.Item>
-            <Menu.Item as="a">2</Menu.Item>
-            <Menu.Item as="a">3</Menu.Item>
-            <Menu.Item as="a">4</Menu.Item>
-            <Menu.Item as="a" icon>
-              <Icon name="chevron right" />
-            </Menu.Item>
-          </Menu>
-        </Table.Footer> */}
         </Table>
       </div>
     )
